a4e49132a06f89b3cf35959f024bcb1c2d7e63f6
Andrew Millar <andrew.millar@qa.com>
2017-12-15 12:10:25 +0000

    Add individual survey page
    Added controller to retrieve survey info by short name
    Added single survey view html and css

--- response_operations_ui/views/surveys.py
+++ response_operations_ui/views/surveys.py
@@ -12,5 +12,27 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 @app.route('/', methods=['GET'])
 def view_surveys():
-    survey_list = survey_controllers.get_surveys_list()
+    # survey_list = survey_controllers.get_surveys_list()
+    survey_list = [
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87",
+            "shortName": "RSI",
+            "longName": "Monthly Business Survey - Retail Sales Index",
+            "surveyRef": "023",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "BRUS",
+            "longName": "Business Register and Umployment Survey",
+            "surveyRef": "222",
+            "legal_basis": "Statistics of Trade Act 1947"
+        }
+    ]
     return render_template('surveys.html', survey_list=survey_list)
+
+
+@app.route('/surveys/<short_name>', methods=['GET'])
+def view_survey(short_name):
+    survey = survey_controllers.get_survey(short_name)
+    return render_template('survey.html', survey=survey)
