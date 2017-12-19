e6d680f745c0b223dfca06e753461556c53d03ac
Andrew Millar <andrew.millar@qa.com>
2017-12-19 16:42:08 +0000

    Add temp json surveys (for demo)

--- response_operations_ui/views/surveys.py
+++ response_operations_ui/views/surveys.py
@@ -12,8 +12,80 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 @app.route('/', methods=['GET'])
 def view_surveys():
-    survey_list = survey_controllers.get_surveys_list()
-    return render_template('surveys.html', survey_list=survey_list)
+    # survey_list = survey_controllers.get_surveys_list()
+    survey_details = [
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef87",
+            "shortName": "RSI",
+            "longName": "Monthly Business Survey - Retail Sales Index",
+            "surveyRef": "023",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "AIFDI",
+            "longName": "Annual Inward Foreign Direct Investment Survey",
+            "surveyRef": "062",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "AOFDI",
+            "longName": "Annual Outward Foreign Direct Investment Survey",
+            "surveyRef": "063",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "QIFDI",
+            "longName": "Quarterly Inward Foreign Direct Investment Survey",
+            "surveyRef": "064",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "QOFDI",
+            "longName": "Quarterly Outward Foreign Direct Investment Survey",
+            "surveyRef": "065",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "Sand&Gravel",
+            "longName": "Quarterly Survey of Building Materials Sand and Gravel",
+            "surveyRef": "066",
+            "legal_basis": "Statistics of Trade Act 1947 - BEIS"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "Blocks",
+            "longName": "Monthly Survey of Building Materials Concrete Building Blocks",
+            "surveyRef": "073",
+            "legal_basis": "Statistics of Trade Act 1947 - BEIS"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "Bricks",
+            "longName": "Monthly Survey of Building Materials Bricks",
+            "surveyRef": "074",
+            "legal_basis": "Voluntary - BEIS"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "MWSS",
+            "longName": "Monthly Wages and Salaries Survey",
+            "surveyRef": "134",
+            "legal_basis": "Statistics of Trade Act 1947"
+        },
+        {
+            "id": "cb0711c3-0ac8-41d3-ae0e-567e5ea1ef88",
+            "shortName": "PCS",
+            "longName": "Public Corporations Survey",
+            "surveyRef": "137",
+            "legal_basis": "Voluntary Not Stated"
+        }
+    ]
+    return render_template('surveys.html', survey_list=survey_details)
 
 
 @app.route('/surveys/<short_name>', methods=['GET'])
