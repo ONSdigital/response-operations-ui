5417743f158cde7b0b647bc4d5a81134fcf9a795
Tejas Patel <tejas.patel@ons.gov.uk>
2018-01-16 16:24:11 +0000

    fix codacy issues

--- response_operations_ui/static/js/read-csv.js
+++ response_operations_ui/static/js/read-csv.js
@@ -1,46 +1,4 @@
 
-function handleFiles(files) {
-	// Check for the various File API support.
-	if (window.FileReader) {
-		// FileReader are supported.
-		getAsText(files[0]);
-	} else {
-		alert('FileReader is not supported in this browser.');
-	}
-}
-
-function getAsText(fileToRead) {
-	var reader = new FileReader();
-	// Handle errors load
-	reader.onload = loadHandler;
-	reader.onerror = errorHandler;
-	// Read file into memory as UTF-8
-	reader.readAsText(fileToRead);
-}
-
-function loadHandler(event) {
-    var csv = event.target.result;
-	processData(csv);
-}
-
-function processData(csv) {
-    var allTextLines = csv.split(/\r\n|\n/);
-    var lines = [];
-
-    while (allTextLines.length) {
-        lines.push(allTextLines.shift().split(':'));
-    }
-
-    drawOutput(lines);
-}
-
-
-function errorHandler(evt) {
-	if(evt.target.error.name == "NotReadableError") {
-		alert("Cannot read file :(");
-	}
-}
-
 function drawOutput(lines){
 
 	// Put the form types into their own separate array, so we can interrogate it faster
@@ -48,8 +6,6 @@ function drawOutput(lines){
 	for (var i = 0; i < lines.length; i++) {
 			formTypes.push(lines[i][lines.length - 2]);
 	}
-
-	//console.log(formTypes);
 
 	var ciCount = formTypes.filter(function(val, i, arr) {
 	    return arr.indexOf(val) === i;
@@ -60,7 +16,7 @@ function drawOutput(lines){
 	document.getElementById("sample-preview").innerHTML = "";
 
 	var preview = "";
-	preview = preview + "<div class='panel panel--info'>"
+	preview = preview + "<div class='panel panel--info'>";
 	preview = preview + "  <div class='panel__header'>";
 	preview = preview + "    <h3 class='venus'>Sample contents</h3>";
 	preview = preview + "  </div>";
@@ -78,7 +34,46 @@ function drawOutput(lines){
 	document.getElementById("sample-collection-instruments").value = ciCount;
 }
 
+function errorHandler(evt) {
+	if(evt.target.error.name == "NotReadableError") {
+		alert("Cannot read file :(");
+	}
+}
 
+function loadHandler(event) {
+    var csv = event.target.result;
+	processData(csv);
+}
+
+function getAsText(fileToRead) {
+	var reader = new FileReader();
+	// Handle errors load
+	reader.onload = loadHandler;
+	reader.onerror = errorHandler;
+	// Read file into memory as UTF-8
+	reader.readAsText(fileToRead);
+}
+
+function handleFiles(files) {
+	// Check for the various File API support.
+	if (window.FileReader) {
+		// FileReader are supported.
+		getAsText(files[0]);
+	} else {
+		alert('FileReader is not supported in this browser.');
+	}
+}
+
+function processData(csv) {
+    var allTextLines = csv.split(/\r\n|\n/);
+    var lines = [];
+
+    while (allTextLines.length) {
+        lines.push(allTextLines.shift().split(":"));
+    }
+
+    drawOutput(lines);
+}
 
 function cancelLoadSample(){
 
