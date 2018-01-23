43d64466068be700a8b542b84e354ea22629e61c
Adam Mann <manny160389@hotmail.com>
2018-01-23 13:54:20 +0000

    Updated login to use the backstage service

--- response_operations_ui/views/sign_in.py
+++ response_operations_ui/views/sign_in.py
@@ -1,4 +1,6 @@
 import logging
+import requests
+import json
 
 from flask import Blueprint, render_template, request
 from flask_login import login_user
@@ -6,6 +8,7 @@ from structlog import wrap_logger
 
 from response_operations_ui.forms import LoginForm
 from response_operations_ui.user import User
+from response_operations_ui import app
 
 logger = wrap_logger(logging.getLogger(__name__))
 
@@ -17,14 +20,22 @@ def login():
     form = LoginForm(request.form)
 
     if form.validate_on_submit():
-        username = request.form.get('username')
-        password = request.form.get('password')
+        sign_in_data = {
+            "username": request.form.get('username'),
+            "password": request.form.get('password'),
+        }
+        url = f'{app.config["BACKSTAGE_API_URL"]}/sign-in-uaa'
+        response = requests.post(url, json=sign_in_data)
+        logger.info(response.text)
+        response = json.loads(response.text)
 
-        user = User(form.id.data)
 
-        if username == 'user' and password == 'pass':
+        if 'token' in response:
+            user = User(form.id.data)
             login_user(user)
+        else:
+            form.username.errors.append(response['error'])
 
-            return render_template('home.html')
+        return render_template('home.html')
 
     return render_template('sign_in.html', form=form)
