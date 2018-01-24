a931a4f50977a1a7446d1740689c4068922d4819
Adam Mann <manny160389@hotmail.com>
2018-01-24 16:16:46 +0000

    Removed currently unused pages

--- response_operations_ui/views/errors.py
+++ response_operations_ui/views/errors.py
@@ -9,12 +9,6 @@ logger = wrap_logger(logging.getLogger(__name__))
 error_bp = Blueprint('error_bp', __name__, template_folder='templates/errors')
 
 
-@error_bp.route('/404', methods=['GET', 'POST'])
-def not_found_error_page():
-    return render_template('errors/404-error.html', _theme='default',
-                           data={"error": {"type": "failed"}}), 404
-
-
 @error_bp.route('/500', methods=['GET', 'POST'])
 def server_error_page():
     return render_template('errors/500-error.html', _theme='default',
