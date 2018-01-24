b0e785f44280cf52f09f25d4335003e97d7ee9df
Adam Mann <manny160389@hotmail.com>
2018-01-24 15:47:33 +0000

    Add simple test for user module

---
+++ tests/test_user.py
@@ -0,0 +1,9 @@
+import unittest
+from response_operations_ui.user import User
+
+
+class TestUser(unittest.TestCase):
+
+    def test_user_id(self):
+        user = User("token123")
+        self.assertEquals(user.id, "token123")
