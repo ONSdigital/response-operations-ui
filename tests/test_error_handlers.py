f083fd19364794f00abeb818e92f8f40f31840de
dcdarrell9 <darrells_king@hotmail.com>
2018-01-26 12:45:08 +0000

    change max line length to 120, and fix flake error

--- tests/test_error_handlers.py
+++ tests/test_error_handlers.py
@@ -12,5 +12,6 @@ class TestErrorHandlers(unittest.TestCase):
     @patch('requests.post')
     def test_exception_error_page(self, mock_post):
         mock_post.side_effect = Exception("error")
-        response = self.app.post('/sign-in', data={'username': 'username', 'password': 'password'}, follow_redirects=True)
+        response = self.app.post('/sign-in', data={'username': 'username', 'password': 'password'},
+                                 follow_redirects=True)
         self.assertIn(b'Something has gone wrong with the website.', response.data)
