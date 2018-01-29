349845e90a91e5fe32ef8c2899b7de8157fa4d52
dcdarrell9 <darrells_king@hotmail.com>
2018-01-29 14:17:06 +0000

    rename get_sign_in, remove unused form data

--- response_operations_ui/controllers/sign_in_controller.py
+++ response_operations_ui/controllers/sign_in_controller.py
@@ -1,4 +1,3 @@
-import json
 import logging
 
 import requests
@@ -9,9 +8,9 @@ from response_operations_ui import app
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-def get_sign_in(sign_in_data):
+def sign_in(sign_in_data):
     logger.debug('Retrieving sign in authorisation')
     url = f'{app.config["BACKSTAGE_API_URL"]}/sign-in-uaa'
 
-    response_json = requests.post(url, json=sign_in_data)
-    return json.loads(response_json.text)
+    response = requests.post(url, json=sign_in_data)
+    return response.json()
--- tests/views/test_sign_in.py
+++ tests/views/test_sign_in.py
@@ -4,7 +4,7 @@ import requests_mock
 
 from response_operations_ui import app
 
-url_get_sign_in_data = f'{app.config["BACKSTAGE_API_URL"]}/sign-in-uaa'
+url_sign_in_data = f'{app.config["BACKSTAGE_API_URL"]}/sign-in-uaa'
 
 
 class TestSignIn(unittest.TestCase):
@@ -22,8 +22,8 @@ class TestSignIn(unittest.TestCase):
         self.assertIn(b'You\'ve logged out', response.data)
 
     @requests_mock.mock()
-    def test_get_sign_in(self, mock_request):
-        mock_request.post(url_get_sign_in_data, json={"token": "1234abc"})
+    def test_sign_in(self, mock_request):
+        mock_request.post(url_sign_in_data, json={"token": "1234abc"})
 
         response = self.app.post("/sign-in", follow_redirects=True, data={"username": "user", "password": "pass"})
 
