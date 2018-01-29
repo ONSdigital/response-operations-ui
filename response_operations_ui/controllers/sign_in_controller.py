3ef4e4e9eca81cb324f48e647b64feab8b68b9e5
dcdarrell9 <darrells_king@hotmail.com>
2018-01-29 15:53:37 +0000

    add error handler for sign in controller

--- response_operations_ui/controllers/sign_in_controller.py
+++ response_operations_ui/controllers/sign_in_controller.py
@@ -4,6 +4,7 @@ import requests
 from structlog import wrap_logger
 
 from response_operations_ui import app
+from response_operations_ui.exceptions.exceptions import ApiError
 
 logger = wrap_logger(logging.getLogger(__name__))
 
@@ -13,5 +14,8 @@ def sign_in(sign_in_data):
     url = f'{app.config["BACKSTAGE_API_URL"]}/sign-in-uaa'
 
     response = requests.post(url, json=sign_in_data)
+    if response.status_code != 201:
+        raise ApiError(response)
+
     logger.debug('Successfully signed in')
     return response.json()
