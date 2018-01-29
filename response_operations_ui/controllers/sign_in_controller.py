ff667d449108be68f96d16329f147c3e18ddf600
dcdarrell9 <darrells_king@hotmail.com>
2018-01-29 16:29:03 +0000

    revert change of v2 to v1 and hardcode version for now on sign in

--- response_operations_ui/controllers/sign_in_controller.py
+++ response_operations_ui/controllers/sign_in_controller.py
@@ -3,7 +3,6 @@ import logging
 import requests
 from structlog import wrap_logger
 
-from response_operations_ui import app
 from response_operations_ui.exceptions.exceptions import ApiError
 
 logger = wrap_logger(logging.getLogger(__name__))
@@ -11,10 +10,10 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 def sign_in(sign_in_data):
     logger.debug('Retrieving sign in authorisation')
-    url = f'{app.config["BACKSTAGE_API_URL"]}/sign-in'
+    url = f'http://localhost:8001/backstage-api/v2/sign-in'
 
     response = requests.post(url, json=sign_in_data)
-    if response.status_code != 200:
+    if response.status_code != 201:
         raise ApiError(response)
 
     logger.debug('Successfully signed in')
