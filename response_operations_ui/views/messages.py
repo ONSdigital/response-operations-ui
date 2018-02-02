8f2886b6cb208b038d9ed28663901795fa55990d
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-02-02 15:17:36 +0000

    added a inbox to the website with hardcoded data

--- response_operations_ui/views/messages.py
+++ response_operations_ui/views/messages.py
@@ -7,16 +7,24 @@ from structlog import wrap_logger
 logger = wrap_logger(logging.getLogger(__name__))
 
 messages_bp = Blueprint("messages_bp", __name__, static_folder='Static',
-                        template_folder='/templates/messages')
+                        template_folder='templates')
+
 
 @messages_bp.route('/', methods=['GET'])
 @login_required
 def view_messages():
-    message_list = {"subject": "subject text",
-                   "ru_id": "12345ab",
-                   "from": "person",
-                   "to": "other person",
-                   "received": "01/01/2018T00:00:00"}
+    message_list = [{"subject": "subject text",
+                    "business": "test123",
+                     "ru_id": "12345ab",
+                     "from": "person",
+                     "to": "other person",
+                     "received": "01/01/2018T00:00:00"},
+                    {"subject": "subject text2",
+                     "business": "QA",
+                     "ru_id": "4567 7 66",
+                     "from": "ONS",
+                     "to": "QA",
+                     "received": "20/06/2017T00:00:00"}]
 
     breadcrumbs = [{"title": "Messages"}]
     return render_template("messages.html", breadcrumbs=breadcrumbs, message_list=message_list)
