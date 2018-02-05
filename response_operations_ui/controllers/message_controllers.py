6fc69e5c8d40245cb2bae097c96104330642cab4
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-02-05 11:54:06 +0000

    added token to controller

--- response_operations_ui/controllers/message_controllers.py
+++ response_operations_ui/controllers/message_controllers.py
@@ -1,5 +1,5 @@
 import logging
-
+import jwt
 import requests
 from structlog import wrap_logger
 
@@ -10,9 +10,11 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 
 def get_message_list():
+
+    #token = jwt.encode
     logger.debug("Retrieving Message list")
     url = f'{app.config["BACKSTAGE_API_URL"]}/v1/secure-message/messages'
-    response = requests.get(url)
+    response = requests.get(url, headers={'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1MTc4Mjk0NTYsImV4cCI6MTU0OTM2NTQ1NiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsInBhcnR5X2lkIjoiQlJFUyIsInJvbGUiOiJpbnRlcm5hbCJ9.8MzPQvULms45V5bGa_DeipNgyYbS_jOeXAcHnq3dImE'})
     if response.status_code != 200:
         raise ApiError(response)
 
--- response_operations_ui/views/messages.py
+++ response_operations_ui/views/messages.py
@@ -4,6 +4,8 @@ from flask import Blueprint, render_template
 from flask_login import login_required
 from structlog import wrap_logger
 
+from response_operations_ui.controllers import message_controllers
+
 logger = wrap_logger(logging.getLogger(__name__))
 
 messages_bp = Blueprint("messages_bp", __name__, static_folder='Static',
@@ -13,18 +15,20 @@ messages_bp = Blueprint("messages_bp", __name__, static_folder='Static',
 @messages_bp.route('/', methods=['GET'])
 @login_required
 def view_messages():
-    message_list = [{"subject": "subject text",
-                    "business": "test123",
-                     "ru_id": "12345ab",
-                     "from": "person",
-                     "to": "other person",
-                     "received": "01/01/2018T00:00:00"},
-                    {"subject": "subject text2",
-                     "business": "QA",
-                     "ru_id": "4567 7 66",
-                     "from": "ONS",
-                     "to": "QA",
-                     "received": "20/06/2017T00:00:00"}]
+    # message_list = [{"subject": "subject text",
+    #                 "business": "test123",
+    #                  "ru_id": "12345ab",
+    #                  "from": "person",
+    #                  "to": "other person",
+    #                  "received": "01/01/2018T00:00:00"},
+    #                 {"subject": "subject text2",
+    #                  "business": "QA",
+    #                  "ru_id": "4567 7 66",
+    #                  "from": "ONS",
+    #                  "to": "QA",
+    #                  "received": "20/06/2017T00:00:00"}]
+
+    message_list = message_controllers.get_message_list()
 
     breadcrumbs = [{"title": "Messages"}]
     return render_template("messages.html", breadcrumbs=breadcrumbs, message_list=message_list)
