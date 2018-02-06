a774a1700595d8d1457bfb1c8c4496121b34761c
Emilio Ward <emilioward@hotmail.co.uk>
2018-02-06 13:35:32 +0000

    map backend states to frontend

--- response_operations_ui/views/surveys.py
+++ response_operations_ui/views/surveys.py
@@ -34,7 +34,28 @@ def view_survey(short_name):
             "title": f"{survey_details['survey']['surveyRef']} {survey_details['survey']['shortName']}",
         }
     ]
+
+    # Mapping backend states to frontend sates for the user
+    for collection_exercise_state in survey_details["collection_exercises"]:
+        if collection_exercise_state["state"] == "INIT":
+            collection_exercise_state["state"] = "Created"
+
+        if collection_exercise_state["state"] == "SCHEDULED":
+            collection_exercise_state["state"] = "Scheduled"
+
+        if collection_exercise_state["state"] == "READY_FOR_REVIEW":
+            collection_exercise_state["state"] = "Ready for review"
+
+        if collection_exercise_state["state"] == "EXECUTION_STARTED":
+            collection_exercise_state["state"] = "Processing"
+
+        if collection_exercise_state["state"] == "READY_FOR_LIVE":
+            collection_exercise_state["state"] = "Live"
+        logger.debug("survey details value {}".format(collection_exercise_state['state']))
+
     return render_template('survey.html',
                            survey=survey_details['survey'],
                            collection_exercises=survey_details['collection_exercises'],
+                           survey_state=collection_exercise_state['state'],
                            breadcrumbs=breadcrumbs)
+
