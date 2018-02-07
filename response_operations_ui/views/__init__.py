a3b560e156332a4b818e45a886d09de92d3acbf1
Tejas Patel <tejas.patel@ons.gov.uk>
2018-02-07 16:15:11 +0000

    add find ru functionality

--- response_operations_ui/views/__init__.py
+++ response_operations_ui/views/__init__.py
@@ -4,6 +4,7 @@ from response_operations_ui.views.errors import error_bp
 from response_operations_ui.views.home import home_bp
 from response_operations_ui.views.info import info_bp
 from response_operations_ui.views.logout import logout_bp
+from response_operations_ui.views.reporting_units import reporting_unit_bp
 from response_operations_ui.views.sign_in import sign_in_bp
 from response_operations_ui.views.surveys import surveys_bp
 
@@ -12,5 +13,6 @@ app.register_blueprint(error_bp, url_prefix='/errors')
 app.register_blueprint(home_bp, url_prefix='/')
 app.register_blueprint(info_bp, url_prefix='/info')
 app.register_blueprint(logout_bp, url_prefix='/logout')
+app.register_blueprint(reporting_unit_bp, url_prefix='/reporting-units')
 app.register_blueprint(sign_in_bp, url_prefix='/sign-in')
 app.register_blueprint(surveys_bp, url_prefix='/surveys')
