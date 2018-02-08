81de1eca7147a642855b06c4cf2484419a52c60c
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-02-08 14:07:18 +0000

    More tests for the messages list

--- tests/views/test_message.py
+++ tests/views/test_message.py
@@ -28,9 +28,31 @@ class TestMessage(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("View messages".encode(), response.data)
 
+# Test showing that the messages list loads into the website and displays User, business name and subject
     @requests_mock.mock()
     def test_Message_list(self, mock_request):
         mock_request.get(get_message_list, json=message_list)
 
         response = self.app.get("/messages")
         self.assertEqual(response.status_code, 200)
+        self.assertIn("Acme Studios LTD".encode(), response.data)
+        self.assertIn("Jordon Dutch".encode(), response.data)
+        self.assertIn("Q3 Statistics".encode(), response.data)
+
+    @requests_mock.mock()
+    def test_message_list_fail(self, mock_request):
+        mock_request.get(get_message_list, status_code=500)
+
+        response = self.app.get("/messages", follow_redirects=True)
+
+        self.assertEqual(response.status_code, 500)
+        self.assertIn("Error 500 - Server error".encode(), response.data)
+
+    @requests_mock.mock()
+    def test_message_list_empty(self, mock_request):
+        mock_request.get(get_message_list, json={"messages": []})
+
+        response = self.app.get("/messages")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("There are no messages for this user".encode(), response.data)
