e59c9b8345ab83c6fb88c9757da5215c78133263
Andrew Millar <andrew.millar@qa.com>
2018-03-07 11:20:38 +0000

    Alter js so reporting unit works without javascript

--- response_operations_ui/static/js/data-panel.js
+++ response_operations_ui/static/js/data-panel.js
@@ -3,12 +3,12 @@ function initDataPanels() {
     for (var i = 0; i < acc.length; i++) {
         // Introduce button if js running, so we can focus on each accordion heading
         var panelTitle = acc[i].innerHTML;
-        acc[i].innerHTML = '<button class="data-panel-header__title">' + panelTitle + '<\/button>';
-
+        acc[i].innerHTML = "<button class='data-panel-header__title'>" + panelTitle + "<\/button>";
+        var panel = acc[i].nextElementSibling;
+        panel.style.display = "none";
         // Add event listener to toggle displaying the data panel
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
-            var panel = this.nextElementSibling;
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
