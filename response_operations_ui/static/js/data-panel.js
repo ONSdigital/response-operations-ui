bd1dd00f42fa351952604f59a6e884d1dd38e567
Andrew Millar <andrew.millar@qa.com>
2018-03-07 11:22:02 +0000

    Add comment to js

--- response_operations_ui/static/js/data-panel.js
+++ response_operations_ui/static/js/data-panel.js
@@ -1,11 +1,12 @@
 function initDataPanels() {
     var acc = document.getElementsByClassName("data-panel-header");
     for (var i = 0; i < acc.length; i++) {
-        // Introduce button if js running, so we can focus on each accordion heading
+        // Introduce button and hide data panel content if js running
         var panelTitle = acc[i].innerHTML;
         acc[i].innerHTML = "<button class='data-panel-header__title'>" + panelTitle + "<\/button>";
         var panel = acc[i].nextElementSibling;
         panel.style.display = "none";
+
         // Add event listener to toggle displaying the data panel
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
