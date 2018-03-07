a6e621b72dc19ff5208eb93a29936505e9ed80d8
Andrew Millar <andrew.millar@qa.com>
2018-03-07 13:53:41 +0000

    Update js to open correct data panel

--- response_operations_ui/static/js/data-panel.js
+++ response_operations_ui/static/js/data-panel.js
@@ -4,11 +4,12 @@ function initDataPanels() {
         // Introduce button and hide data panel content if js running
         var panelTitle = acc[i].innerHTML;
         acc[i].innerHTML = "<button class='data-panel-header__title'>" + panelTitle + "<\/button>";
-        var panel = acc[i].nextElementSibling;
-        panel.style.display = "none";
-
+        var initPanel = acc[i].nextElementSibling;
+        initPanel.style.display = "none";
+        
         // Add event listener to toggle displaying the data panel
         acc[i].addEventListener("click", function() {
+            var panel = this.nextElementSibling;
             this.classList.toggle("active");
             if (panel.style.display === "block") {
                 panel.style.display = "none";
