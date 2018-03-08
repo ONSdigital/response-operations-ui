6a9aa1d7751a281e1c7862e6b0a196a06cb12b33
Andrew Millar <andrew.millar@qa.com>
2018-03-08 13:54:58 +0000

    Stop injecting html with js

--- response_operations_ui/static/js/data-panel.js
+++ response_operations_ui/static/js/data-panel.js
@@ -2,8 +2,7 @@ function initDataPanels() {
     var acc = document.getElementsByClassName("data-panel-header");
     for (var i = 0; i < acc.length; i++) {
         // Introduce button and hide data panel content if js running
-        var panelTitle = acc[i].innerHTML;
-        acc[i].innerHTML = "<button class='data-panel-header__title'>" + panelTitle + "<\/button>";
+
         var initPanel = acc[i].nextElementSibling;
         initPanel.style.display = "none";
 
