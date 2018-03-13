b0d24273b70ce740bbb30f1ca244099f7064a3ac
Adam <35268982+AdamHawtin@users.noreply.github.com>
2018-03-13 11:22:18 +0000

    SM111 Update Inbox View to Show Threads (#86)
    * Changed inbox to get threads, updated UI

--- response_operations_ui/controllers/survey_controllers.py
+++ response_operations_ui/controllers/survey_controllers.py
@@ -30,3 +30,14 @@ def get_survey(short_name):
 
     logger.debug('Successfully retrieved survey', short_name=short_name)
     return response.json()
+
+
+def get_survey_short_name_by_id(survey_id):
+    # TODO cache the surveys dictionary at app start up
+    try:
+        surveys_dict = {survey['id']: survey for survey in get_surveys_list()}
+        return surveys_dict[survey_id]['shortName']
+    except ApiError:
+        logger.exception("Failed to resolve survey short name due to API error", survey_id=survey_id)
+    except KeyError:
+        logger.exception("Failed to resolve survey short name", survey_id=survey_id)
