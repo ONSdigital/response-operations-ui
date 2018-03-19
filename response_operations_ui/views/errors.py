0a55d7ef14f45733f260f78586c5663420e624d5
rhiGriff <rhiannongriffiths73@gmail.com>
2018-03-19 13:28:50 +0000

    Us058 update respondent email (#101)
    * Add old and new email address to backstage call
    
    * Use hidden field to reduce API calls in backstage
    
    * Handle detail changes in UI
    
    * adding messages and changing email address
    
    * adding new email address
    
    * adding new email address to logic
    
    * adding unit tests
    
    * completing unit tests
    
    * starting to address PR comments by moving the if statements from the views to the controller
    
    * refactor and add error handler

--- response_operations_ui/views/errors.py
+++ response_operations_ui/views/errors.py
@@ -2,7 +2,7 @@ import logging
 
 from flask import Blueprint, render_template
 from structlog import wrap_logger
-
+from response_operations_ui.exceptions.exceptions import UpdateContactDetailsException
 
 logger = wrap_logger(logging.getLogger(__name__))
 
@@ -12,3 +12,16 @@ error_bp = Blueprint('error_bp', __name__, template_folder='templates/errors')
 @error_bp.route('/500', methods=['GET'])
 def server_error_page():
     return render_template('errors/500-error.html'), 500
+
+
+@error_bp.app_errorhandler(UpdateContactDetailsException)
+def update_details_exception(error=None):
+
+    logger.error('update details error', ru_ref=error.ru_ref, status_code=error.status_code)
+    error_type = 'email conflict' if error.status_code == 409 else 'api error'
+
+    return render_template('edit-contact-details.html',
+                           ru_ref=error.ru_ref,
+                           form=error.form,
+                           error_type=error_type,
+                           respondent_details=error.respondent_details)
