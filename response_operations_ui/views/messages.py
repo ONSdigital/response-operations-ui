3c008abb2fc60949c8720a585f696f197189583d
James Gardiner <james@jgardiner.co.uk>
2018-03-21 11:39:17 +0000

    First attempt at reply

--- response_operations_ui/views/messages.py
+++ response_operations_ui/views/messages.py
@@ -16,7 +16,6 @@ logger = wrap_logger(logging.getLogger(__name__))
 messages_bp = Blueprint('messages_bp', __name__,
                         static_folder='static', template_folder='templates')
 
-
 @messages_bp.route('/create-message', methods=['POST'])
 @login_required
 def create_message():
@@ -47,71 +46,44 @@ def create_message():
                            form=form,
                            breadcrumbs=breadcrumbs)
 
-
-def _build_create_message_breadcrumbs():
-    return [
-        {"title": "Messages", "link": "/messages"},
-        {"title": "Create Message"}
-    ]
-
-
-def _get_conversation_breadcrumbs(messages):
-    return [
-        {"title": "Messages", "link": "/messages"},
-        {"title": messages[-1].get('subject', 'No Subject')}
-    ]
-
-
-def _repopulate_form_with_submitted_data(form):
-    form.survey.text = form.hidden_survey.data
-    form.ru_ref.text = form.hidden_ru_ref.data
-    form.business.text = form.hidden_business.data
-    form.to.text = form.hidden_to.data
-    form.body.data = g.form_body_data
-    form.subject.data = g.form_subject_data
-    return form
-
-
-def _get_message_json(form):
-    return json.dumps({
-        'msg_from': current_user.id,
-        'msg_to': [form.hidden_to_uuid.data],
-        'subject': form.subject.data,
-        'body': form.body.data,
-        'thread_id': "",
-        'collection_case': "",
-        # TODO Make this UUID for v2 api
-        'survey': form.hidden_survey_id.data,
-        'ru_id': form.hidden_to_ru_id.data})
-
-
-def _populate_hidden_form_fields_from_post(current_view_form, calling_form):
-    """
-    :param current_view_form: is the form just create when land in the view
-    :param calling_form: is the form that is actually sent from the caller
-    :return: a form with all the hidden data filled in.
-    """
+@messages_bp.route('/threads/<thread_id>', methods=['GET', 'POST'])
+@login_required
+def view_conversation(thread_id):
     try:
-        current_view_form.hidden_survey.data = calling_form['survey']
-        current_view_form.hidden_survey_id.data = calling_form['survey_id']
-        current_view_form.hidden_ru_ref.data = calling_form['ru_ref']
-        current_view_form.hidden_business.data = calling_form['business']
-        current_view_form.hidden_to_uuid.data = calling_form['msg_to']
-        current_view_form.hidden_to.data = calling_form['msg_to_name']
-        current_view_form.hidden_to_ru_id.data = calling_form['ru_id']
-    except KeyError as ex:
-        logger.exception("Failed to load create message page")
-        raise InternalError(ex)
-    return current_view_form
+        thread_conversation = message_controllers.get_conversation(thread_id)['messages']
+        breadcrumbs = _get_conversation_breadcrumbs(thread_conversation)
+        refined_thread = [_refine(message) for message in reversed(thread_conversation)]
+    except KeyError as e:
+        logger.exception("A key error occurred")
+        raise ApiError(e)
+    except IndexError:
+        breadcrumbs = [
+            {"title": "Messages", "link": "/messages"},
+            {"title": "Unavailable"}
+        ]
 
+    form = SecureMessageForm(request.form)
+    if form.validate_on_submit():
+        form = _populate_form_details_from_hidden_fields(form)
+        g.form_subject_data = form.subject.data
+        g.form_body_data = form.body.data
 
-def _populate_form_details_from_hidden_fields(form):
-    form.survey.text = form.hidden_survey.data
-    form.ru_ref.text = form.hidden_ru_ref.data
-    form.business.text = form.hidden_business.data
-    form.to.text = form.hidden_to.data
-    return form
+        try:
+            message_controllers.send_message(_get_message_json(form))
+            survey = request.form.get("hidden_survey")
+            flash("Message sent.")
+            return redirect(url_for('messages_bp.view_selected_survey', selected_survey=survey))
+        except (ApiError, InternalError):
+            form = _repopulate_form_with_submitted_data(form)
+            form.errors['sending'] = ["Message failed to send, something has gone wrong with the website."]
+            return render_template('create-message.html',
+                                   form=form,
+                                   breadcrumbs=breadcrumbs)
 
+    return render_template("conversation-view.html",
+                           breadcrumbs=breadcrumbs,
+                           messages=refined_thread,
+                           form=SecureMessageForm(request.form))
 
 @messages_bp.route('/', methods=['GET', 'POST'])
 @login_required
@@ -173,25 +145,70 @@ def view_selected_survey(selected_survey):
                                response_error=True)
 
 
-@messages_bp.route('/threads/<thread_id>', methods=['GET'])
-@login_required
-def view_conversation(thread_id):
+
+def _build_create_message_breadcrumbs():
+    return [
+        {"title": "Messages", "link": "/messages"},
+        {"title": "Create Message"}
+    ]
+
+
+def _get_conversation_breadcrumbs(messages):
+    return [
+        {"title": "Messages", "link": "/messages"},
+        {"title": messages[-1].get('subject', 'No Subject')}
+    ]
+
+
+def _repopulate_form_with_submitted_data(form):
+    form.survey.text = form.hidden_survey.data
+    form.ru_ref.text = form.hidden_ru_ref.data
+    form.business.text = form.hidden_business.data
+    form.to.text = form.hidden_to.data
+    form.body.data = g.form_body_data
+    form.subject.data = g.form_subject_data
+    return form
+
+
+def _get_message_json(form):
+    return json.dumps({
+        'msg_from': current_user.id,
+        'msg_to': [form.hidden_to_uuid.data],
+        'subject': form.subject.data,
+        'body': form.body.data,
+        'thread_id': "",
+        'collection_case': "",
+        # TODO Make this UUID for v2 api
+        'survey': form.hidden_survey_id.data,
+        'ru_id': form.hidden_to_uuid.data})
+
+
+def _populate_hidden_form_fields_from_post(current_view_form, calling_form):
+    """
+    :param current_view_form: is the form just create when land in the view
+    :param calling_form: is the form that is actually sent from the caller
+    :return: a form with all the hidden data filled in.
+    """
     try:
-        thread_conversation = message_controllers.get_conversation(thread_id)['messages']
-        breadcrumbs = _get_conversation_breadcrumbs(thread_conversation)
-        refined_thread = [_refine(message) for message in reversed(thread_conversation)]
+        current_view_form.hidden_survey.data = calling_form['survey']
+        current_view_form.hidden_survey_id.data = calling_form['survey_id']
+        current_view_form.hidden_ru_ref.data = calling_form['ru_ref']
+        current_view_form.hidden_business.data = calling_form['business']
+        current_view_form.hidden_to_uuid.data = calling_form['msg_to']
+        current_view_form.hidden_to.data = calling_form['msg_to_name']
+        current_view_form.hidden_to_ru_id.data = calling_form['ru_id']
+    except KeyError as ex:
+        logger.exception("Failed to load create message page")
+        raise InternalError(ex)
+    return current_view_form
 
-    except KeyError as e:
-        logger.exception("A key error occurred")
-        raise ApiError(e)
-    except IndexError:
-        breadcrumbs = [
-            {"title": "Messages", "link": "/messages"},
-            {"title": "Unavailable"}
-        ]
 
-    return render_template("conversation-view.html", breadcrumbs=breadcrumbs, messages=refined_thread)
-
+def _populate_form_details_from_hidden_fields(form):
+    form.survey.text = form.hidden_survey.data
+    form.ru_ref.text = form.hidden_ru_ref.data
+    form.business.text = form.hidden_business.data
+    form.to.text = form.hidden_to.data
+    return form
 
 def _get_message_subject(thread):
     try:
@@ -216,7 +233,8 @@ def _refine(message):
         'business_name': _get_business_name_from_message(message),
         'from': _get_from_name(message),
         'to': _get_to_name(message),
-        'sent_date': _get_human_readable_date(message.get('sent_date'))
+        'sent_date': _get_human_readable_date(message.get('sent_date')),
+        'ru_id': message.get('ru_id')
     }
 
 
