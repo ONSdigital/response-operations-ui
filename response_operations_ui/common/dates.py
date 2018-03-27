686e0c5ef0d3436e59bf07c59a151c698283df42
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-03-27 12:55:26 +0100

    Improve docstring, improved exception log output

--- response_operations_ui/common/dates.py
+++ response_operations_ui/common/dates.py
@@ -8,13 +8,14 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 def get_formatted_date(datetime_string, string_format='%Y-%m-%d %H:%M:%S'):
     """Takes a string date in given format returns a string 'today', 'yesterday' at the time in format '%H:%M'
-    if the given date is today or yesterday respectively otherwise returns the full date in the format '%b %d %Y %H:%M'
+    if the given date is today or yesterday respectively otherwise returns the full date in the format '%b %d %Y %H:%M'.
+    If datetime_string is not a valid date in the given format it is returned with no formatting.
     """
     try:
         datetime_parsed = datetime.strptime(datetime_string, string_format)
     except (OverflowError, ValueError, AttributeError):
         # Passed value wasn't date-ish or date arguments out of range
-        logger.exception("Failed to parse date from message", sent_date=datetime_string)
+        logger.exception("Failed to parse date", sent_date=datetime_string)
         return datetime_string
 
     time_difference = datetime.date(datetime_parsed) - date.today()
