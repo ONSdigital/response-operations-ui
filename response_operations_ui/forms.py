346e74a962e3437da3cc2b5447901ebe9ca9efa2
Andrew Millar <andrew.millar@qa.com>
2018-04-23 13:25:58 +0100

    Add extra options for hour selector

--- response_operations_ui/forms.py
+++ response_operations_ui/forms.py
@@ -84,7 +84,8 @@ class UpdateEventDateForm(FlaskForm):
                        validators=[InputRequired(message="Please enter year"),
                                    Length(min=4, max=4, message="Please enter a 4 digit number")])
 
-    HOURS = [(hour, hour) for hour in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']]
+    HOURS = [(hour, hour) for hour in ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11',
+                                       '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']]
     hour = SelectField('hours', choices=HOURS)
 
     MINUTES = [('00', '00'), ('15', '15'), ('30', '30'), ('45', '45')]
