e2a0c95040d6f7dd0263c1c640e55e7fa2ef537a
Andrew Millar <andrew.millar@qa.com>
2018-04-23 11:15:29 +0100

    Update date parsing

--- response_operations_ui/common/mappers.py
+++ response_operations_ui/common/mappers.py
@@ -15,15 +15,15 @@ def format_short_name(short_name):
 def convert_events_to_new_format(events):
     formatted_events = {}
     for event in events:
-        date_time = iso8601.parse_date(event['timestamp'])
+        date_time = iso8601.parse_date(event['timestamp'], )
         day = calendar.day_name[date_time.weekday()]
-        month = calendar.month_name[date_time.month][:3]
-        date = f"{date_time.strftime('%d')} {month} {date_time.strftime('%Y')}"
-        time = f"{date_time.strftime('%H:%M')} GMT"
+        # month = calendar.month_name[date_time.month][:3]
+        date = date_time.strftime('%d %b %Y')
+        time = date_time.strftime('%H:%M GMT')
         formatted_events[event['tag']] = {
             "day": day,
             "date": date,
-            "month": str(date_time.month) if len(str(date_time.month)) == 2 else f"0{date_time.month}",
+            "month": date_time.strftime('%m'),
             "time": time,
             "is_in_future": date_time > iso8601.parse_date(datetime.now().isoformat())
         }
