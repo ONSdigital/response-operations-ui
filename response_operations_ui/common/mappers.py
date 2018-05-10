5aa6abbcece6934695e8e19d212cab56c1161a5a
Andrew Millar <andrew.millar@qa.com>
2018-05-10 12:16:43 +0100

    Fix getting of day from datetime

--- response_operations_ui/common/mappers.py
+++ response_operations_ui/common/mappers.py
@@ -16,7 +16,7 @@ def convert_events_to_new_format(events):
     formatted_events = {}
     for event in events:
         date_time = iso8601.parse_date(event['timestamp'])
-        day = calendar.day_name[date_time.strftime('%A')]
+        day = date_time.strftime('%A')
         date = date_time.strftime('%d %b %Y')
         time = date_time.strftime('%H:%M GMT')
         formatted_events[event['tag']] = {
