e17ec37345fb1c98792023e23ac3be35e0675c20
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-05-11 16:08:09 +0100

    Session cleared on logout and added test

--- response_operations_ui/views/logout.py
+++ response_operations_ui/views/logout.py
@@ -8,16 +8,6 @@ logout_bp = Blueprint('logout_bp', __name__, static_folder='static', template_fo
 @logout_bp.route('/')
 def logout():
     logout_user()
-
-    try:
-        del (session['messages_survey_selection'])
-    except KeyError:
-        pass
-
-    try:
-        del (session['next'])
-    except KeyError:
-        pass
-
+    session.clear()
     flash("You are now signed out", category='successful_signout')
     return redirect(url_for('sign_in_bp.sign_in'))
