0bf19ad2378a35e2089d4a5cae04707c8bb96476
Richard Weeks <richard.weeks@ons.gov.uk>
2018-05-17 10:34:54 +0100

    Extract backend endpoint for sample upload.

--- response_operations_ui/controllers/collection_exercise_controllers.py
+++ response_operations_ui/controllers/collection_exercise_controllers.py
@@ -164,3 +164,27 @@ def get_case_group_status_by_collection_exercise(case_groups, collection_exercis
 def get_collection_exercise_from_list(exercises, period):
     return next((exercise for exercise in exercises
                  if exercise['exerciseRef'] == period), None)
+
+
+def link_sample_summary_to_collection_exercise(collection_exercise_id, sample_summary_id):
+    logger.debug('Linking sample summary to collection exercise',
+                 collection_exercise_id=collection_exercise_id,
+                 sample_summary_id=sample_summary_id)
+    url = f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises/link/{collection_exercise_id}'
+    # Currently we only need to link a single sample to a single collection exercise
+    payload = {'sampleSummaryIds': [str(sample_summary_id)]}
+    response = requests.put(url, auth=app.config['COLLECTION_EXERCISE_AUTH'], json=payload)
+
+    if response.status_code == 404:
+        logger.error('Error retrieving collection exercise', collection_exercise_id=collection_exercise_id)
+        raise ApiError(url, response.status_code)
+    if response.status_code != 200:
+        logger.error('Error linking sample to collection exercise',
+                     collection_exercise_id=collection_exercise_id,
+                     sample_summary_id=sample_summary_id)
+        raise ApiError(url, response.status_code)
+
+    logger.debug('Successfully linked sample summary with collection exercise',
+                 collection_exercise_id=collection_exercise_id,
+                 sample_summary_id=sample_summary_id)
+    return response.json()
--- response_operations_ui/controllers/sample_controllers.py
+++ response_operations_ui/controllers/sample_controllers.py
@@ -1,35 +1,63 @@
 import logging
 
 import requests
+
+from flask import jsonify, make_response, request
+
 from requests.exceptions import HTTPError, RequestException
 from structlog import wrap_logger
 
 from response_operations_ui import app
 from response_operations_ui.exceptions.exceptions import ApiError
+from response_operations_ui.controllers import collection_exercise_controllers, survey_controllers
+from response_operations_ui.common.filters import get_collection_exercise_by_period
 
 logger = wrap_logger(logging.getLogger(__name__))
 
-def upload_sample(collection_exercise_id, sample_file, survey_type='B'):
-    logger.debug('Uploading sample file',
-                 collection_exercise_id=collection_exercise_id,
-                 survey_type=survey_type)
+
+def upload_sample(short_name, period, file):
+    logger.debug('Uploading sample', short_name=short_name, period=period, filename=file.filename)
+
+    survey = survey_controllers.get_survey_by_shortname(short_name)
+    exercises = collection_exercise_controllers.get_collection_exercises_by_survey(survey['id'])
+    # Find the collection exercise for the given period
+    exercise = get_collection_exercise_by_period(exercises, period)
+    if not exercise:
+        return make_response(jsonify({'message': 'Collection exercise not found'}), 404)
+
+    survey_type = 'B'
     url = f'{app.config["SAMPLE_URL"]}/samples/{survey_type}/fileupload'
-    response = requests.post(url=url, auth=app.config['SAMPLE_AUTH'], files={'file': sample_file})
+    response = requests.post(url=url, auth=app.config['SAMPLE_AUTH'], files={'file': file})
 
     try:
         response.raise_for_status()
     except (HTTPError, RequestException):
         log_level = logger.warning if response.status_code in (400, 404) else logger.exception
         log_level('Error uploading sample file',
-                     collection_exercise_id=collection_exercise_id,
+                     collection_exercise_id=exercise['id'],
                      status=response.status_code,
                      survey_type=survey_type)
         raise ApiError(response)
 
-    response_json = response.json()
+    sample_summary = response.json()
 
     logger.debug('Successfully uploaded sample file',
-                 collection_exercise_id=collection_exercise_id,
-                 response_json=response_json,
+                 collection_exercise_id=exercise['id'],
+                 response_json=sample_summary,
                  survey_type=survey_type)
-    return response_json
+
+    logger.info('Successfully uploaded sample', sample_id=sample_summary['id'])
+
+    logger.info('Linking sample summary with collection exercise',
+                collection_exercise_id=exercise['id'],
+                sample_id=sample_summary['id'])
+
+    collection_exercise_controllers.link_sample_summary_to_collection_exercise(
+        collection_exercise_id=exercise['id'],
+        sample_summary_id=sample_summary['id'])
+
+    logger.info('Successfully linked sample to collection exercise',
+                collection_exercise_id=exercise['id'],
+                sample_id=sample_summary['id'])
+
+    return sample_summary
