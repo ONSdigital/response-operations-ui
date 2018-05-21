eda4fcd3518e1193ad04fc65ab0cab2c289782d0
madeye-matt <matt@madeye.com>
2018-05-21 13:47:04 +0100

    P2 load sample timeout (#156)
    * Added refresh link to sample section
    
    * Implemented preliminary RAD
    
    - permanent status information in sample section
    - transient sample upload information above H1
    
    * Made blue "processing" h1 message transient
    
    * Changed 'Sample successfully loaded' to 'Sample loaded successfully'

--- tests/views/test_collection_exercise.py
+++ tests/views/test_collection_exercise.py
@@ -265,7 +265,7 @@ class TestCollectionExercise(unittest.TestCase):
         json_date = {
             "sampleSummaryPK": 1,
             "id": "d7d13200-34a1-4a66-9f3b-ea0af4bc023d",
-            "state": "INIT",
+            "state": "ACTIVE",
             "ingestDateTime": "2017-11-06T14:02:24.203+0000"
         }
 
@@ -275,7 +275,7 @@ class TestCollectionExercise(unittest.TestCase):
         response = self.app.post("/surveys/test/000000", data=post_data)
 
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Sample successfully loaded".encode(), response.data)
+        self.assertIn("Sample loaded successfully".encode(), response.data)
         self.assertIn("Loaded sample summary".encode(), response.data)
         self.assertIn('2\n'.encode(), response.data)
         self.assertIn('5\n'.encode(), response.data)
@@ -306,7 +306,7 @@ class TestCollectionExercise(unittest.TestCase):
         response = self.app.post("/surveys/test/000000", data=data)
 
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn("Sample successfully loaded".encode(), response.data)
+        self.assertNotIn("Sample loaded successfully".encode(), response.data)
         self.assertNotIn("Loaded sample summary".encode(), response.data)
 
     @requests_mock.mock()
@@ -317,7 +317,7 @@ class TestCollectionExercise(unittest.TestCase):
         response = self.app.post("/surveys/test/000000", data=data)
 
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn("Sample successfully loaded".encode(), response.data)
+        self.assertNotIn("Sample loaded successfully".encode(), response.data)
         self.assertNotIn("Loaded sample summary".encode(), response.data)
 
     @requests_mock.mock()
@@ -329,7 +329,7 @@ class TestCollectionExercise(unittest.TestCase):
         response = self.app.post('/surveys/test/000000', data=post_data)
 
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn('Sample successfully loaded'.encode(), response.data)
+        self.assertNotIn('Sample loaded successfully'.encode(), response.data)
         self.assertIn('Collection exercise executed'.encode(), response.data)
         self.assertIn('Processing collection exercise'.encode(), response.data)
 
@@ -342,7 +342,7 @@ class TestCollectionExercise(unittest.TestCase):
         response = self.app.post('/surveys/test/000000', data=post_data)
 
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn('Sample successfully loaded'.encode(), response.data)
+        self.assertNotIn('Sample loaded successfully'.encode(), response.data)
         self.assertNotIn('Collection exercise executed'.encode(), response.data)
         self.assertIn('Failed to execute Collection Exercise'.encode(), response.data)
 
