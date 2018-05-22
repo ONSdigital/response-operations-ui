b7c247b28cc8d1e5fafc237f43a660e94db1b95c
Richard Weeks <richard.weeks@ons.gov.uk>
2018-05-22 14:20:26 +0100

    Remove unused code.

--- response_operations_ui/common/filters.py
+++ response_operations_ui/common/filters.py
@@ -2,9 +2,3 @@ def get_collection_exercise_by_period(exercises, period):
     for exercise in exercises:
         if exercise['exerciseRef'] == period:
             return exercise
-
-
-def get_case_group_status_by_collection_exercise(case_groups, collection_exercise_id):
-    for case_group in case_groups:
-        if case_group['collectionExerciseId'] == collection_exercise_id:
-            return case_group['caseGroupStatus']
