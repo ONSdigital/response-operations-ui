0f80eb427977bc78fdfefd3e5c50d04a1692f4d0
James Gardiner <james@jgardiner.co.uk>
2018-06-15 13:23:13 +0100

    Reformat using Black

--- response_operations_ui/controllers/collection_exercise_controllers.py
+++ response_operations_ui/controllers/collection_exercise_controllers.py
@@ -39,14 +39,20 @@ def download_report(collection_exercise_id, survey_id):
 
 
 def get_collection_exercise(short_name, period):
-    logger.debug("Retrieving collection exercise details", short_name=short_name, period=period)
-    url = f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/{short_name}/{period}'
+    logger.debug(
+        "Retrieving collection exercise details", short_name=short_name, period=period
+    )
+    url = (
+        f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/{short_name}/{period}'
+    )
     response = requests.get(url)
     if response.status_code != 200:
         raise ApiError(response)
 
     logger.debug(
-        "Successfully retrieved collection exercise details", short_name=short_name, period=period
+        "Successfully retrieved collection exercise details",
+        short_name=short_name,
+        period=period,
     )
     return response.json()
 
@@ -57,7 +63,9 @@ def get_collection_exercise_event_page_info(short_name, period):
         short_name=short_name,
         period=period,
     )
-    url = f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/{short_name}/{period}/events'
+    url = (
+        f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/{short_name}/{period}/events'
+    )
     response = requests.get(url)
     if response.status_code != 200:
         raise ApiError(response)
@@ -91,13 +99,17 @@ def update_event(short_name, period, tag, timestamp):
     elif response.status_code != 201:
         raise ApiError(response)
 
-    logger.debug("Successfully updated event date", short_name=short_name, period=period, tag=tag)
+    logger.debug(
+        "Successfully updated event date", short_name=short_name, period=period, tag=tag
+    )
     return True
 
 
 def execute_collection_exercise(short_name, period):
     logger.debug("Executing collection exercise", short_name=short_name, period=period)
-    url = f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/{short_name}/{period}/execute'
+    url = (
+        f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/{short_name}/{period}/execute'
+    )
     response = requests.post(url)
     if response.ok:
         logger.debug(
@@ -107,19 +119,27 @@ def execute_collection_exercise(short_name, period):
         )
         return True
 
-    logger.debug("Failed to execute collection exercise", short_name=short_name, period=period)
-
-
-def update_collection_exercise_details(collection_exercise_id, user_description, period):
     logger.debug(
-        "Updating collection exercise details", collection_exercise_id=collection_exercise_id
+        "Failed to execute collection exercise", short_name=short_name, period=period
+    )
+
+
+def update_collection_exercise_details(
+    collection_exercise_id, user_description, period
+):
+    logger.debug(
+        "Updating collection exercise details",
+        collection_exercise_id=collection_exercise_id,
     )
     url = (
         f'{app.config["BACKSTAGE_API_URL"]}/v1/collection-exercise/'
-        f'update-collection-exercise-details/{collection_exercise_id}'
+        f"update-collection-exercise-details/{collection_exercise_id}"
     )
 
-    collection_exercise_details = {"user_description": user_description, "period": period}
+    collection_exercise_details = {
+        "user_description": user_description,
+        "period": period,
+    }
 
     response = requests.put(url, json=collection_exercise_details)
     if response.status_code != 200:
@@ -132,8 +152,12 @@ def update_collection_exercise_details(collection_exercise_id, user_description,
 
 
 def get_collection_exercise_by_id(collection_exercise_id):
-    logger.debug("Retrieving collection exercise", collection_exercise_id=collection_exercise_id)
-    url = f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises/{collection_exercise_id}'
+    logger.debug(
+        "Retrieving collection exercise", collection_exercise_id=collection_exercise_id
+    )
+    url = (
+        f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises/{collection_exercise_id}'
+    )
     response = requests.get(url, auth=app.config["COLLECTION_EXERCISE_AUTH"])
 
     try:
@@ -141,19 +165,23 @@ def get_collection_exercise_by_id(collection_exercise_id):
     except requests.exceptions.HTTPError:
         log_level = logger.warning if response.status_code == 404 else logger.exception
         log_level(
-            "Failed to retrieve collection exercise", collection_exercise_id=collection_exercise_id
+            "Failed to retrieve collection exercise",
+            collection_exercise_id=collection_exercise_id,
         )
         raise ApiError(response)
 
     logger.debug(
-        "Successfully retrieved collection exercise", collection_exercise_id=collection_exercise_id
+        "Successfully retrieved collection exercise",
+        collection_exercise_id=collection_exercise_id,
     )
     return response.json()
 
 
 def create_collection_exercise(survey_id, survey_name, user_description, period):
     logger.debug(
-        "Creating a new collection exercise for", survey_id=survey_id, survey_name=survey_name
+        "Creating a new collection exercise for",
+        survey_id=survey_id,
+        survey_name=survey_name,
     )
     header = {"Content-Type": "application/json"}
     url = f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises'
@@ -178,13 +206,17 @@ def create_collection_exercise(survey_id, survey_name, user_description, period)
         raise ApiError(response)
 
     logger.debug(
-        "Successfully created collection exercise for", survey_id=survey_id, survey_name=survey_name
+        "Successfully created collection exercise for",
+        survey_id=survey_id,
+        survey_name=survey_name,
     )
 
 
 def get_collection_exercises_by_survey(survey_id):
     logger.debug("Retrieving collection exercises", survey_id=survey_id)
-    url = f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises/survey/{survey_id}'
+    url = (
+        f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises/survey/{survey_id}'
+    )
 
     response = requests.get(url, auth=app.config["COLLECTION_EXERCISE_AUTH"])
 
@@ -194,10 +226,14 @@ def get_collection_exercises_by_survey(survey_id):
     try:
         response.raise_for_status()
     except HTTPError:
-        logger.exception("Failed to retrieve collection exercises by survey", survey_id=survey_id)
+        logger.exception(
+            "Failed to retrieve collection exercises by survey", survey_id=survey_id
+        )
         return ApiError(response)
 
-    logger.debug("Successfully retrieved collection exercises by survey", survey_id=survey_id)
+    logger.debug(
+        "Successfully retrieved collection exercises by survey", survey_id=survey_id
+    )
     return response.json()
 
 
@@ -216,7 +252,7 @@ def unlink_sample_summary(collection_exercise_id, sample_summary_id):
         sample_summary_id=sample_summary_id,
     )
     url = f'{app.config["COLLECTION_EXERCISE_URL"]}/collectionexercises/unlink/'
-    url += f'{collection_exercise_id}/sample/{sample_summary_id}'
+    url += f"{collection_exercise_id}/sample/{sample_summary_id}"
 
     response = requests.delete(url, auth=app.config["COLLECTION_EXERCISE_AUTH"])
 
@@ -239,10 +275,14 @@ def unlink_sample_summary(collection_exercise_id, sample_summary_id):
 
 
 def get_collection_exercise_from_list(exercises, period):
-    return next((exercise for exercise in exercises if exercise["exerciseRef"] == period), None)
+    return next(
+        (exercise for exercise in exercises if exercise["exerciseRef"] == period), None
+    )
 
 
-def link_sample_summary_to_collection_exercise(collection_exercise_id, sample_summary_id):
+def link_sample_summary_to_collection_exercise(
+    collection_exercise_id, sample_summary_id
+):
     logger.debug(
         "Linking sample summary to collection exercise",
         collection_exercise_id=collection_exercise_id,
@@ -253,7 +293,9 @@ def link_sample_summary_to_collection_exercise(collection_exercise_id, sample_su
     )
     # Currently we only need to link a single sample to a single collection exercise
     payload = {"sampleSummaryIds": [str(sample_summary_id)]}
-    response = requests.put(url, auth=app.config["COLLECTION_EXERCISE_AUTH"], json=payload)
+    response = requests.put(
+        url, auth=app.config["COLLECTION_EXERCISE_AUTH"], json=payload
+    )
 
     try:
         response.raise_for_status()
