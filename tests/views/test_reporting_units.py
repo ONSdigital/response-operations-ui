a1582d4a93e69bada27f77039be959654d3666a4
Jamie Cox <me@jcox.tech>
2018-06-19 09:44:08 +0100

    Fix lint error

--- tests/views/test_reporting_units.py
+++ tests/views/test_reporting_units.py
@@ -23,7 +23,8 @@ url_search_reporting_units = f'{backstage_api_url}/v1/reporting-unit/search'
 get_respondent_by_id_url = f'{app.config["PARTY_URL"]}/party-api/v1/respondents/id/{respondent_party_id}'
 url_edit_contact_details = f'{app.config["PARTY_URL"]}/party-api/v1/respondents/id/{respondent_party_id}'
 url_generate_new_code = f'{backstage_api_url}/v1/reporting-unit/iac/ce_id/ru_ref'
-url_resend_verification_email = f'{app.config["PARTY_URL"]}/party-api/v1/resend-verification-email/{respondent_party_id}'
+url_resend_verification_email = f'{app.config["PARTY_URL"]}/party-api/v1/resend-verification-email' \
+                                f'/{respondent_party_id}'
 url_change_enrolment_status = f'{app.config["BACKSTAGE_API_URL"]}/v1/party/change-enrolment-status'
 
 url_get_party_by_ru_ref = f'{app.config["PARTY_URL"]}/party-api/v1/parties/type/B/ref/{ru_ref}'
