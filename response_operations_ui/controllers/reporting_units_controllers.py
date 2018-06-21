1db63e0d61062ac78f905b4b37be4b6bae459a49
Jamie <me@jcox.tech>
2018-06-21 10:43:52 +0100

    [TBL] Call party direct when resending verification email (#181)
    * Change to call party direct when resending verification email

--- response_operations_ui/controllers/reporting_units_controllers.py
+++ response_operations_ui/controllers/reporting_units_controllers.py
@@ -56,10 +56,12 @@ def generate_new_enrolment_code(collection_exercise_id, ru_ref):
 
 def resend_verification_email(party_id):
     logger.debug('Re-sending verification email', party_id=party_id)
-    url = f'{app.config["BACKSTAGE_API_URL"]}/v1/reporting-unit/resend-verification-email/{party_id}'
-    response = requests.post(url)
+    url = f'{app.config["PARTY_URL"]}/party-api/v1/resend-verification-email/{party_id}'
+    response = requests.get(url, auth=app.config['PARTY_AUTH'])
 
-    if response.status_code != 200:
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
         logger.exception("Re-sending of verification email failed", party_id=party_id)
         raise ApiError(response)
 
