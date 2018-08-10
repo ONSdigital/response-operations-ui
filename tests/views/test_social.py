8885e5232a3dc98201d6ba4e1bc8c74d18e7454a
damien-lloyd <damien.lloyd21@gmail.com>
2018-08-10 10:38:33 +0100

    Removed global app/client vars from test

--- tests/views/test_social.py
+++ tests/views/test_social.py
@@ -5,9 +5,6 @@ from response_operations_ui import create_app
 from config import TestingConfig
 from tests.views import ViewTestCase
 
-app = create_app('TestingConfig')
-client = app.test_client()
-
 with open('tests/test_data/case/case_details.json') as f:
     case_details = json.load(f)
 
@@ -41,7 +38,7 @@ class TestSocial(ViewTestCase):
         # Mock getting cases based on returned samples
         mock_request.get(CASE_SAMPLES_URL, status_code=200, json=case_details)
 
-        response = client.post("/social", data={'query': pc})
+        response = self.client.post("/social", data={'query': pc})
 
         self.assertEqual(response.status_code, 200)
 
@@ -53,7 +50,7 @@ class TestSocial(ViewTestCase):
 
         mock_request.get(postcode_url(pc), status_code=404)
 
-        response = client.post("/social", data={'query': pc})
+        response = self.client.post("/social", data={'query': pc})
 
         self.assertEqual(response.status_code, 200)
         self.assertIn(f'0 results found for {pc}', str(response.data))
