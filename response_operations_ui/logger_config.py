0ebdb0a2f5aa627539546ba5fafd955ce552f9a0
Richard Weeks <richard.weeks@ons.gov.uk>
2018-09-05 15:07:11 +0100

    Add Zipkin IDs to log.

--- response_operations_ui/logger_config.py
+++ response_operations_ui/logger_config.py
@@ -2,6 +2,8 @@ import logging
 import os
 import sys
 
+import flask
+from flask g
 from structlog import configure
 from structlog.processors import format_exc_info, JSONRenderer, TimeStamper
 from structlog.stdlib import add_log_level, filter_by_level
@@ -32,7 +34,18 @@ def logger_initial_config(service_name=None,
         event_dict['service'] = service_name
         return event_dict
 
+    def zipkin_ids(logger, method_name, event_dict):
+        event_dict['zipkin_trace_id'] = ''
+        event_dict['zipkin_span_id'] = ''
+        if not flask.has_app_context():
+            return event_dict
+        if '_zipkin_span' not in g:
+            return event_dict
+        event_dict['zipkin_span_id'] = g._zipkin_span.zipkin_attrs.span_id
+        event_dict['zipkin_trace_id'] = g._zipkin_span.zipkin_attrs.trace_id
+        return event_dict
+
     logging.basicConfig(stream=sys.stdout, level=log_level, format=logger_format)
-    configure(processors=[add_log_level, filter_by_level, add_service, format_exc_info,
+    configure(processors=[zipkin_ids, add_log_level, filter_by_level, add_service, format_exc_info,
                           TimeStamper(fmt=logger_date_format, utc=True, key="created_at"),
                           JSONRenderer(indent=indent)])
