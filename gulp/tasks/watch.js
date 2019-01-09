27b45863bea47baead9919e5a4f3e4d810c466fe
Owen C. Jones <owencjones@gmail.com>
2019-01-09 16:34:56 +0000

    Fix for watcher

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -27,7 +27,7 @@ function taskFunction(context, callback) {
         throw (new GulpError('JS_DEST_DIR config setting not found'));
     }
 
-    gulp.watch(SCSS_DIR + '**/*.{css, scss}', series('csslint', 'scsscompile', () => callback()));
+    gulp.watch(SCSS_DIR + '/**/*.scss', series('csslint', 'scsscompile', () => callback()));
     gulp.watch([JS_SRC_DIR + '/**/*.js', PROJECT_ROOT + '/gulpfile.js', PROJECT_ROOT + '/gulp/**/*.js'], series('jslint', () => callback()));
 }
 
