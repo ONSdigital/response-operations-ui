104271ad955131188aa8a227bb56f47c134bba1c
Matthew Crocker <matt.crocker@gmail.com>
2019-02-20 15:21:10 +0000

    Remove TBL123 feature flagging

--- response_operations_ui/views/__init__.py
+++ response_operations_ui/views/__init__.py
@@ -9,12 +9,10 @@ from response_operations_ui.views.reporting_units import reporting_unit_bp
 from response_operations_ui.views.respondents import respondent_bp
 from response_operations_ui.views.sign_in import sign_in_bp
 from response_operations_ui.views.surveys import surveys_bp
+from response_operations_ui.views import update_event_date
 
 
 def setup_blueprints(app):
-    # Only enable the edit event dates endpoints if flag is present
-    if app.config['EDIT_EVENT_DATES_ENABLED']:
-        from response_operations_ui.views import update_event_date  # NOQA
     app.register_blueprint(collection_exercise_bp, url_prefix='/surveys')
     app.register_blueprint(error_bp, url_prefix='/errors')
     app.register_blueprint(home_bp, url_prefix='/')
