d7b4be3f0976b88e9758ef74eb20e6b4002965ce
Owen C. Jones <owencjones@gmail.com>
2019-02-25 11:46:24 +0000

    Mock respondents use actual data file

--- response_operations_ui/controllers/mock_party_controller.py
+++ response_operations_ui/controllers/mock_party_controller.py
@@ -1,10 +1,12 @@
-def mock_search_respondents(first_name, last_name, email_address, page=0): 
-    return [{
-            'id': '1',
-            'firstName': 'Bob',
-            'lastName': 'Saget',
-            'emailAddress': 'bsaget@saget.com',
-            'sampleUnitType': 'BI',
-            'status': 'ACTIVE',
-            'telephone': '01234567890'
-            }]
+import json
+
+
+def mock_search_respondents(first_name, last_name, email_address, page=1):
+    with open('response_operations_ui/data/mock_search_respondents_data.json', 'r') as json_data:
+        data = json.load(json_data)
+        data_chunks = [data[i * 25:(i + 1) * 25] for i in range((len(data) + 25 - 1) // 25)]
+
+        return {
+            'data': data_chunks[int(page) - 1],
+            'total': len(data)
+        }
