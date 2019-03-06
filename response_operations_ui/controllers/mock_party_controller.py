d71ea45aa9bb192901cdd0f89ffd0c24f46bfc0c
Owen C. Jones <owencjones@gmail.com>
2019-03-06 12:28:27 +0000

    Added unit tests to mock controller, not yet passing

--- response_operations_ui/controllers/mock_party_controller.py
+++ response_operations_ui/controllers/mock_party_controller.py
@@ -9,20 +9,24 @@ def mock_search_respondents(first_name, last_name, email_address, page=1):
         limit = app.config['PARTY_RESPONDENTS_PER_PAGE']
 
         chunk_range = range((len(data) + limit - 1) // limit)
-        sorted_data = sorted(data, key=lambda dict: dict["lastName"].lower())
-
-        if first_name:
-            sorted_data = [a for a in sorted_data if a['firstName'].lower().startswith(first_name.lower())]
-
-        if last_name:
-            sorted_data = [a for a in sorted_data if a['lastName'].lower().startswith(last_name.lower())]
-
-        if email_address:
-            sorted_data = [a for a in sorted_data if email_address.lower() in a['emailAddress']]
-
+        data_by_surname = sorted(data, key=lambda dict: dict["lastName"].lower())
+        sorted_data = filter_json_by_passed_parameters(data_by_surname, first_name, last_name, email_address)
         data_chunks = [sorted_data[i * limit:(i + 1) * limit] for i in chunk_range]
 
         return {
             'data': data_chunks[int(page) - 1],
             'total': len(sorted_data)
         }
+
+
+def filter_json_by_passed_parameters(sorted_data, first_name='', last_name='', email_address=''):
+    if first_name:
+        sorted_data = [a for a in sorted_data if a['firstName'].lower().startswith(first_name.lower())]
+
+    if last_name:
+        sorted_data = [a for a in sorted_data if a['lastName'].lower().startswith(last_name.lower())]
+
+    if email_address:
+        sorted_data = [a for a in sorted_data if email_address.lower() in a['emailAddress']]
+
+    return sorted_data
