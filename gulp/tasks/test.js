0a20205071287e8b633e6e6aa9a8624ddbbd243f
Owen C. Jones <owencjones@gmail.com>
2019-03-29 13:46:46 +0000

    Gulp Jest Runner Changes

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,15 +1,5 @@
 const { registerTask } = require('../gulpHelper');
-const jest = require('gulp-jest').default;
-const { join } = require('path');
-
-function taskFunction() {
-    const config = this.config;
-    const jestConf = require(join(config.PROJECT_ROOT, 'package.json')).jest;
-    const testMatchGlobs = jestConf.testMatch.map(match => `${config.PROJECT_ROOT}/${match}`);
-
-    return this.gulp.src(testMatchGlobs).pipe(jest(jestConf));
-}
 
 module.exports = (context) => {
-    registerTask(context, ['test'], taskFunction.bind(context));
+    registerTask(context, ['test'], context.gulp.series(['csslint', 'jslint', 'jstest']));
 };
