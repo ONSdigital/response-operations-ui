42a2cfd681a323400e8d42e2d7d663bcf538bbf2
Owen Jones <owencjones+github@gmail.com>
2019-04-29 15:06:24 +0100

    New JS tests for data panel (#311)
    * Tests working
    
    * PR comments addressed

---
+++ response_operations_ui/assets/tests/data-panel.test.js
@@ -0,0 +1,71 @@
+require('../../static/js/data-panel');
+const { defer } = require('lodash');
+
+describe('Data panel tests', () => {
+    let dataPanelContainer;
+    let dataPanelHeader;
+    const dataPanelMarkup = `
+    <div class="data-panel" id="survey">
+        <h3 class="data-panel-header">
+            <button class='data-panel-header__title'></button>
+        </h3>
+
+        <div class="data-panel-body">
+            <p>Some content</p>
+        </div>
+    </div>
+    `;
+
+    beforeEach(() => {
+        dataPanelContainer = document.createElement('div');
+        dataPanelContainer.id = 'dataPanelContainer';
+        dataPanelContainer.innerHTML = dataPanelMarkup;
+        document.body.append(dataPanelContainer);
+
+        dataPanelHeader = document.querySelector('.data-panel-header');
+        dataPanelBody = document.querySelector('.data-panel-body');
+    });
+
+    afterEach(() => {
+        dataPanelContainer.remove();
+    });
+
+    describe('Behaviour without panels initialised', () => {
+        test('Contents of panels is visible', () => {
+            expect(dataPanelBody.style.display).toBe('');
+        });
+
+        test('Content visible after clicking panel toggle', () => {
+            dataPanelHeader.click();
+            expect(dataPanelBody.style.display).toBe('');
+        });
+    });
+
+    describe('Behaviour with panels initialised', () => {
+        beforeEach(() => {
+            initDataPanels();
+        });
+
+        test('Panels initialise with content hidden', () => {
+            expect(dataPanelBody.style.display).toBe('none');
+        });
+
+        test('Clicking panel toggle first time shows panel contents', () => {
+            dataPanelBody.click();
+            defer(() => {
+                expect(dataPanelBody.style.display).toBe('block');
+            });
+        });
+
+        test('Clicking panel contents twice hides panel contents', () => {
+            dataPanelBody.click();
+            defer(() => {
+                dataPanelBody.click();
+                expect(dataPanelBody.style.display).toBe('block');
+                defer(() => {
+                    expect(dataPanelBody.style.display).toBe('none');
+                });
+            });
+        });
+    });
+});
