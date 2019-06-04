221fe21843fd96f46ef579654fe73f4850e5759b
Owen Jones <owencjones@gmail.com>
2019-06-04 13:08:20 +0100

    Js test read csv (#318)
    * WIP:Refactoring JS
    
    * First function tested
    
    * WIP: Setup for another test
    
    * WIP irritatingly mysterious test fails
    
    * Incidental package updates
    
    * Finally found issue with the test
    
    * Added tests for error handler
    
    * Added test for browserHasFileLoaderCapability
    
    * Linting change
    
    * Factoring out JQuery dependency
    
    * cancelSampleLoad tests
    
    * Tests for processFile in place
    
    * WIP Final test
    
    * WIP Final test FileReader mock in progress
    
    * Final test in place and working
    
    * CSS recompile - incidental to ticket
    
    * Couple of errors fixed
    
    * changed view to use newly enclosed function
    
    * Lint error

--- response_operations_ui/static/js/read-csv.js
+++ response_operations_ui/static/js/read-csv.js
@@ -1,90 +1,91 @@
-(function(window) {
-    function drawOutput(businessCount, ciCount) {
-        // Clear previous data
-        document.getElementById('sample-preview').innerHTML = '';
+window.readCSV = {
+    __private__: {}
+};
 
-        let preview = '';
-        preview += '<div class=\'panel panel--info\'>';
-        preview += '  <div class=\'panel__header\'>';
-        preview += '    <h3 class=\'u-fs-r--b\'>Sample contents</h3>';
-        preview += '  </div>';
-        preview += '  <div class=\'panel__body\'>';
-        preview += '    <div id=\'sample-preview-businesses\'>Number of businesses: ' + businessCount + '</div>';
-        preview += '    <div id=\'sample-preview-ci\'>Collection instruments: ' + ciCount + '</div>';
-        preview += '  </div>';
-        preview += '</div>';
+window.readCSV.__private__.getOutputTemplate = function(businessCount, ciCount) {
+    let preview = '';
 
-        document.getElementById('sample-preview').innerHTML = preview;
-        document.getElementById('btn-check-sample-contents').style.display = 'none';
-        document.getElementById('btn-load-sample').style.display = 'inline-block';
-        document.getElementById('btn-cancel-load-sample').style.display = 'inline-block';
+    preview += '<div class=\'panel panel--info\'>';
+    preview += '  <div class=\'panel__header\'>';
+    preview += '    <h3 class=\'u-fs-r--b\'>Sample contents</h3>';
+    preview += '  </div>';
+    preview += '  <div class=\'panel__body\'>';
+    preview += '    <div id=\'sample-preview-businesses\'>Number of businesses: ' + businessCount + '</div>';
+    preview += '    <div id=\'sample-preview-ci\'>Collection instruments: ' + ciCount + '</div>';
+    preview += '  </div>';
+    preview += '</div>';
+
+    return preview;
+};
+
+window.readCSV.__private__.renderUI = function(businessCount, ciCount) {
+    document.getElementById('sample-preview').innerHTML = window.readCSV.__private__.getOutputTemplate(businessCount, ciCount);
+    document.getElementById('btn-check-sample-contents').style.display = 'none';
+    document.getElementById('btn-load-sample').style.display = 'inline-block';
+    document.getElementById('btn-cancel-load-sample').style.display = 'inline-block';
+};
+
+window.readCSV.__private__.errorHandler = function(evt) {
+    if (evt.target.error.name == 'NotReadableError') {
+        alerts.error('Error Uploading file', 'The selected file was not readable.');
     }
+};
 
-    function errorHandler(evt) {
-        if (evt.target.error.name == 'NotReadableError') {
-            alerts.error('Error Uploading file', 'The selected file was not readable.');
+window.readCSV.__private__.browserHasFileLoaderCapability = function() {
+    return window.hasOwnProperty('FileReader');
+};
+
+window.readCSV.__private__.cancelLoadSample = function() {
+    document.getElementById('sample-preview').innerHTML = '';
+    document.getElementById('form-load-sample').reset();
+    document.getElementById('btn-check-sample-contents').style.display = 'block';
+    document.getElementById('btn-load-sample').style.display = 'none';
+    document.getElementById('btn-cancel-load-sample').style.display = 'none';
+    document.getElementById('file').focus();
+};
+
+window.readCSV.__private__.processFile = function(event, classifiers) {
+    const csv = event.target.result;
+    const allTextLines = csv.split(/\r\n|\n/);
+    const classifierColumn = [];
+    const lines = [];
+    let ciCount;
+
+    while (allTextLines.length) {
+        const line = allTextLines.shift().split(':');
+        if (line && line.length && line[0]) {
+            lines.push(line);
         }
     }
 
-    function handleFiles(files, classifiers) {
-        if (_browserHasFileLoaderCapability()) {
-            const reader = new FileReader();
-
-            reader.onload = function(evt) {
-                processFile(evt, classifiers);
-            };
-
-            reader.onerror = errorHandler;
-
-            reader.readAsText(files[0]);
-        } else {
-            alerts.warn('Your browser does not support file uploading.  Uploading of samples will not be possible.');
-        }
-    }
-
-    function processFile(event, classifiers) {
-        const csv = event.target.result;
-        const allTextLines = csv.split(/\r\n|\n/);
-        const classifierColumn = [];
-        const lines = [];
-        let ciCount;
-
-        while (allTextLines.length) {
-            const line = allTextLines.shift().split(':');
-            if (line && line.length && line[0]) {
-                lines.push(line);
-            }
+    if (classifiers.indexOf('RU_REF') > -1) {
+        ciCount = lines.length; // each line should be a distinct RU_REF (sampleUnitRef)
+    } else if (classifiers.indexOf('FORM_TYPE') > -1) {
+        // Put the form types into their own separate array, so we can interrogate it faster
+        for (let i = 0; i < lines.length; i++) {
+            classifierColumn.push(lines[i][lines[i].length - 2]);
         }
 
-        if (classifiers.indexOf('RU_REF') > -1) {
-            ciCount = lines.length; // each line should be a distinct RU_REF (sampleUnitRef)
-        } else if (classifiers.indexOf('FORM_TYPE') > -1) {
-            // Put the form types into their own separate array, so we can interrogate it faster
-            for (let i = 0; i < lines.length; i++) {
-                classifierColumn.push(lines[i][lines[i].length - 2]);
-            }
-
-            ciCount = classifierColumn.filter(function(val, i, arr) {
-                return arr.indexOf(val) === i;
-            }).length;
-        }
-
-        drawOutput(lines.length, ciCount);
+        ciCount = classifierColumn.filter(function(val, i, arr) {
+            return arr.indexOf(val) === i;
+        }).length;
     }
 
-    function cancelLoadSample() {
-        document.getElementById('sample-preview').innerHTML = '';
-        document.getElementById('form-load-sample').reset();
-        document.getElementById('btn-check-sample-contents').style.display = 'block';
-        document.getElementById('btn-load-sample').style.display = 'none';
-        document.getElementById('btn-cancel-load-sample').style.display = 'none';
-        $('#file').focus();
-    }
+    window.readCSV.__private__.renderUI(lines.length, ciCount);
+};
 
-    function _browserHasFileLoaderCapability() {
-        return window.hasOwnProperty('FileReader');
-    }
+window.readCSV.handleFiles = function(files, classifiers) {
+    if (window.readCSV.__private__.browserHasFileLoaderCapability()) {
+        const reader = new FileReader();
 
-    window.handleFiles = handleFiles;
-    window.cancelLoadSample = cancelLoadSample;
-}(window));
+        reader.onload = function(evt) {
+            window.readCSV.__private__.processFile(evt, classifiers);
+        };
+
+        reader.onerror = window.readCSV.__private__.errorHandler;
+
+        reader.readAsText(files[0]);
+    } else {
+        alerts.warn('Your browser does not support file uploading.  Uploading of samples will not be possible.');
+    }
+};
