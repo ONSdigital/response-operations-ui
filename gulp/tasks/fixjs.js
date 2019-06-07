ac49ba31fd0a8d39760021217d21b567fc999f83
Owen C. Jones <owencjones@gmail.com>
2019-06-07 12:05:14 +0100

    Fixed error in fixjs to allow overwrite

--- gulp/tasks/fixjs.js
+++ gulp/tasks/fixjs.js
@@ -5,11 +5,12 @@ const { get } = require('lodash');
 
 function taskFunction() {
     const PROJECT_ROOT = get(this, 'config.PROJECT_ROOT');
-    const jsPath = join(PROJECT_ROOT, 'assets', '**/*.js');
+    const jsPath = join(PROJECT_ROOT, 'assets', 'js');
+    const jsPathGlob = join(jsPath, '**/*.js');
 
-    return this.gulp.src(jsPath)
+    return this.gulp.src(jsPathGlob)
         .pipe(prettier())
-        .pipe(this.gulp.dest());
+        .pipe(this.gulp.dest(jsPath, { overwrite: true }));
 }
 
 module.exports = (context) => {
