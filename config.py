f6a2bdc01852c961f11e13a224dc692cd1ccf4d4
Matt Crocker <matt.crocker@gmail.com>
2019-09-26 09:33:41 +0100

    Not understanding the difference between seconds and minutes

--- config.py
+++ config.py
@@ -92,7 +92,7 @@ class Config(object):
     UAA_CLIENT_SECRET = os.getenv('UAA_CLIENT_SECRET')
 
     EMAIL_TOKEN_SALT = os.getenv('EMAIL_TOKEN_SALT', 'aardvark')
-    EMAIL_TOKEN_EXPIRY = int(os.getenv('EMAIL_TOKEN_EXPIRY', 24*60))
+    EMAIL_TOKEN_EXPIRY = int(os.getenv('EMAIL_TOKEN_EXPIRY', 24*60*60))
 
 
 class DevelopmentConfig(Config):
