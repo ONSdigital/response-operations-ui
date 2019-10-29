d469dc49c35cf8c2a629cb151142abdf9efd9490
Matt Simon <info@boxadesign.co.uk>
2019-10-29 10:38:45 +0000

    Linting

--- response_operations_ui/common/jinja_filters.py
+++ response_operations_ui/common/jinja_filters.py
@@ -9,9 +9,9 @@ def setAttribute(dictionary, key, value):
     dictionary[key] = value
     return dictionary
 
+
 @filter_blueprint.app_template_filter()
 def setAttributes(dictionary, attributes):
     for key in attributes:
         dictionary[key] = attributes[key]
     return dictionary
-
