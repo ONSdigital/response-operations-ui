6d434c3dd7912ca1ed99587d0afa40f553569970
Matt Simon <info@boxadesign.co.uk>
2019-10-29 15:31:47 +0000

    Fixed layout issues with checkboxes and downloads on collection exercise

--- response_operations_ui/common/jinja_filters.py
+++ response_operations_ui/common/jinja_filters.py
@@ -8,3 +8,10 @@ filter_blueprint = flask.Blueprint('filters', __name__)
 def setAttribute(dictionary, key, value):
     dictionary[key] = value
     return dictionary
+
+
+@filter_blueprint.app_template_filter()
+def setAttributes(dictionary, attributes):
+    for key in attributes:
+        dictionary[key] = attributes[key]
+    return dictionary
