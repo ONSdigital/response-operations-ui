9df6637b1490a684d05a0d49b8796439b9060622
Matt Simon <info@boxadesign.co.uk>
2019-11-07 13:01:32 +0000

    Refactor collection exercise (#364)
    * Added latest cdn release of pattern lib, added correct header, breadcrumb markup, code clean up
    
    * Updating templates with pattern library v1.5.2 markup
    
    * fix broken spoke pages and more clean up
    
    * Updated to PL v1.5.3
    
    * Load design system macros, updated base/shell
    
    * Refactored sign-in page to use macros
    
    * updated gitignore to ignore design system files
    
    * Updated surveys template to use macros
    
    * Update gitignore
    
    * Removed design system files
    
    * Updated base template to include correct asset references
    
    * Updated tables to use design system css
    
    * Updated messages nav and collection exercise
    
    * Updated button components for ce templates
    
    * Updated button component across all non-spoke templates
    
    * Updated tables mainly
    
    * Added load-templates to travis
    
    * Added load-templates script to Dockerfile
    
    * Updated to use latest design system release v13.8.0
    
    * Updated build files for loading templates from design system
    
    * Updated formatting for importing blueprint
    
    * Refactored survey template
    
    * Updated to latest design system version
    
    * Fixes from review and upgraded to latest design system release
    
    * Updated grid for collection exercise
    
    * Fixed issue with create message not being sent correct data
    
    * Removed col-6 from flashed messages panel
    
    * Made exerciseRef conditional
    
    * Fixed grid for collection exercise
    
    * Refactored ce-success-panel.html
    
    * Refactored ce-handle-errors.html template
    
    * Refactored ce-header-section.html
    
    * Progress on ce-dates-section.html template
    
    * Fixed duplication in makefile
    
    * Refactoring dates section
    
    * Refactored the ce-dates-section template to be more DRY
    
    * Removed design system components from gitignore and build process
    
    * Removed load-templates from make build
    
    * Updated to latest design system release
    
    * updated base to new pageConfig
    
    * Fix button for reopen conversation
    
    * Updated rectored template
    
    * Added name attr to buttons on collection exercise
    
    * Simplified table component
    
    * Added more name attributes to buttons
    
    * Finalised ce-dates-section template
    
    * Upgraded to design system 14.1.0
    
    * Upgraded to design system 14.2.1 and added required name attributes
    
    * refactored ce-sample-section
    
    * Added table component to ce-sample-section
    
    * Refactor ce-instrument-section
    
    * Updated ce-instrument-section template
    
    * Finished ce-reference-information template refactor
    
    * Linting
    
    * Updated to fix failings from tests
    
    * Added text align to surveys list edit button cell
    
    * Fixes from tests
    
    * Updated metadata for tests
    
    * Added name to survey.html tds
    
    * Added id's to survey.html metadata
    
    * Added ID to error header
    
    * Added id to table for collection instruments
    
    * Removed unnecessary grid from respondent.html
    
    * Typo on class
    
    * Updated id for telephone on edit contact details
    
    * Added required attributes back to edit contact details
    
    * Updated id on edit contact details
    
    * Added Flash messages back in
    
    * Added name values to ce-dates
    
    * Added more name values to ce-dates
    
    * lint fixes

--- response_operations_ui/views/respondents.py
+++ response_operations_ui/views/respondents.py
@@ -137,21 +137,27 @@ def view_contact_details(respondent_id):
 @respondent_bp.route('/edit-contact-details/<respondent_id>', methods=['POST'])
 @login_required
 def edit_contact_details(respondent_id):
-    form = EditContactDetailsForm(form=request.form)
-    if not form.validate():
+    edit_contact_details_form = EditContactDetailsForm(form=request.form)
+    if not edit_contact_details_form.validate():
         contact_details = party_controller.get_respondent_by_party_id(respondent_id)
 
-        return render_template('edit-contact-details.html', form=form, tab='respondents',
-                               respondent_id=respondent_id, errors=form.errors,
+        return render_template('edit-contact-details.html', form=edit_contact_details_form, tab='respondents',
+                               respondent_id=respondent_id, errors=edit_contact_details_form.errors,
                                respondent_details=contact_details)
 
-    else:
-        logger.info('Updating respondent details', respondent_id=respondent_id)
-        form = request.form
-        party_controller.update_contact_details(respondent_id, form)
+    logger.info('Updating respondent details', respondent_id=respondent_id)
+    form = request.form
+    contact_details_changed = party_controller.update_contact_details(respondent_id, form)
 
-        return redirect(url_for('respondent_bp.respondent_details', respondent_id=respondent_id,
-                                message_key='details_changed'))
+    if 'emailAddress' in contact_details_changed:
+        flash(f'Contact details changed and verification email sent to {form.get("email")}')
+    elif len(contact_details_changed) > 0:
+        flash('Contact details changed')
+    else:
+        flash('No updates were necessary')
+
+    return redirect(url_for('respondent_bp.respondent_details', respondent_id=respondent_id,
+                            message_key='details_changed'))
 
 
 @respondent_bp.route('/resend_verification/<respondent_id>', methods=['GET'])
