d46f4370a60ab9abcc66dab5086bd7204b04d47a
Matt Crocker <matt.crocker@gmail.com>
2019-12-03 15:23:02 +0000

    More tests

--- tests/views/test_accounts.py
+++ tests/views/test_accounts.py
@@ -4,11 +4,14 @@ import requests_mock
 
 from response_operations_ui import create_app
 from config import TestingConfig
+from response_operations_ui.common import token_decoder
 
 test_email = "fake@ons.gov.uk"
 url_uaa_token = f"{TestingConfig.UAA_SERVICE_URL}/oauth/token"
 url_uaa_get_accounts = f"{TestingConfig.UAA_SERVICE_URL}/Users?filter=email+eq+%22{test_email}%22"
-url_send_notify = f'{TestingConfig().NOTIFY_SERVICE_URL}{TestingConfig().NOTIFY_REQUEST_CREATE_ACCOUNT_TEMPLATE}'
+url_uaa_create_account = f"{TestingConfig.UAA_SERVICE_URL}/Users"
+url_send_request_notify = f'{TestingConfig().NOTIFY_SERVICE_URL}{TestingConfig().NOTIFY_REQUEST_CREATE_ACCOUNT_TEMPLATE}'
+url_send_created_notify = f'{TestingConfig().NOTIFY_SERVICE_URL}{TestingConfig().NOTIFY_CONFIRM_CREATE_ACCOUNT_TEMPLATE}'
 
 
 class TestAccounts(unittest.TestCase):
@@ -17,6 +20,7 @@ class TestAccounts(unittest.TestCase):
         payload = {'user_id': 'test-id',
                    'aud': 'response_operations'}
         app = create_app('TestingConfig')
+        token = token_decoder.generate_email_token(email)
         self.access_token = jwt.encode(payload, app.config['UAA_PRIVATE_KEY'], algorithm='RS256')
         self.client = app.test_client()
 
@@ -28,7 +32,7 @@ class TestAccounts(unittest.TestCase):
 
     @requests_mock.mock()
     def test_request_account(self, mock_request):
-        mock_request.post(url_send_notify, json={'emailAddress': test_email}, status_code=201)
+        mock_request.post(url_send_request_notify, json={'emailAddress': test_email}, status_code=201)
         mock_request.post(url_uaa_token, json={"access_token": self.access_token.decode()}, status_code=201)
         mock_request.get(url_uaa_get_accounts, json={"totalResults": 0}, status_code=200)
         response = self.client.post("/account/request-new-account", follow_redirects=True,
@@ -55,4 +59,31 @@ class TestAccounts(unittest.TestCase):
                                     data={"email_address": test_email,
                                           "password": TestingConfig.CREATE_ACCOUNT_ADMIN_PASSWORD})
         self.assertIn(b'An account already exists for the email fake@ons.gov.uk', response.data)
+        self.assertEqual(response.status_code, 200)
+    
+    @requests_mock.mock()
+    def test_create_account_page():
+        response = self.client.get(f'/account/create-account/{token}')
+        self.assertIn(b'Consider using your ONS username', response.data)
+        self.assertIn(b'at least one capital letter', response.data)
+        self.assertEqual(response.status_code, 200)
+    
+    @requests_mock.mock()
+    def test_create_account_page_dodgy_token():
+        response = self.cleint.get(f'/account/create-account/dodgy-token')
+        self.assertIn(b'Your link has expired', response.data)
+        self.assertEqual(response.status_code, 200)
+
+    @requests_mock.mock()
+    def test_create_account(self, mock_request):
+        mock_request.post(url_send_created_notify, json={'emailAddress': test_email}, status_code=201)
+        mock_request.post(url_uaa_token, json={"access_token": self.access_token.decode()}, status_code=201)
+        mock_request.post(url_uaa_create_account, json={}, status_code=201)
+        response = self.client.post(f"/account/create-account/{token}", follow_redirects=True,
+                                    data={"password": 'TestPassword1!',
+                                          "user_name": 'testname',
+                                          "first_name": 'Test',
+                                          "last_name": 'Account'})
+        self.assertIn(b'Account successfully created', response.data)
+        self.assertIn(b'Sign in', response.data)
         self.assertEqual(response.status_code, 200)
