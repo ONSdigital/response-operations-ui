8c3a6a42030ff7a2564cdf45d63669c1a92fc7a6
jterryons <john.terry@ons.gov.uk>
2020-04-09 17:47:52 +0100

    fix blank line lint issues

--- response_operations_ui/common/validators.py
+++ response_operations_ui/common/validators.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 from wtforms import ValidationError
-
 from config import Config
+
 
 def valid_date_for_event(tag, form):
     form_datetime = datetime(int(form.year.data),
@@ -12,7 +12,6 @@ def valid_date_for_event(tag, form):
                              int(form.minute.data))
     tags_can_be_in_past = ("ref_period_start", "ref_period_end", "employment")
 
-    
     if not Config.TEST_MODE:
         if tag not in tags_can_be_in_past and form_datetime < datetime.now():
             raise ValidationError('Selected date can not be in the past')
