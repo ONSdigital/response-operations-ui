8f05dce739e0704b13b4485e5ccc52e80f7d1271
anwilkie <47449873+anwilkie@users.noreply.github.com>
2020-04-10 10:15:10 +0100

    added flag for allowing past dates (#450)
    * added flag for allowing past dates
    
    * moved flag to config.py
    
    * fix blank line lint issues
    
    * add test enabled to helm chart
    
    * add secure cookie option
    
    * option to disable csrf protection for testing
    
    * fix signin position
    
    * revert previous
    
    * revert wft_forms var
    
    Co-authored-by: jterryons <john.terry@ons.gov.uk>

--- config.py
+++ config.py
@@ -100,6 +100,8 @@ class Config(object):
     # 6 weeks in seconds
     CREATE_ACCOUNT_EMAIL_TOKEN_EXPIRY = int(os.getenv('CREATE_ACCOUNT_EMAIL_TOKEN_EXPIRY', '3628800'))
     CREATE_ACCOUNT_ADMIN_PASSWORD = os.getenv('CREATE_ACCOUNT_ADMIN_PASSWORD')
+    
+    TEST_MODE = strtobool(os.getenv('TEST_MODE', 'False'))
 
 
 class DevelopmentConfig(Config):
