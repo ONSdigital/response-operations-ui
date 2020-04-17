f186385804a7d0dea31f992b01ff6c4201bdcd5c
bkhan2 <mohammed.khan@ons.gov.uk>
2020-04-17 13:12:00 +0100

    updated the logging to be more descriptive and concise

--- response_operations_ui/controllers/notify_controller.py
+++ response_operations_ui/controllers/notify_controller.py
@@ -39,15 +39,14 @@ class NotifyController:
         url = f'{self.notify_url}{template_id}'
         auth = app.config['SECURITY_USER_NAME'], app.config['SECURITY_USER_PASSWORD']
         response = requests.post(url, auth=auth, json=notification)
-        status_number = response.status_code
+        status_code = response.status_code
 
-        if status_number == 201:
+        if status_code == 201:
             logger.info('Notification id sent via Notify-Gateway to GOV.UK Notify.', id=response.json().get("id"))
         else:
             ref = reference if reference else 'reference_unknown'
-            logger.error(f'STATUS CODE: {status_number}')
-            logger.error(f'URL: {url}')
-            raise NotifyError(f'FAILED TO SEND EMAIL TO: {notification["emailAddress"]}', reference=ref)
+            raise NotifyError(f'There was a problem sending a notification to Notify-Gateway. STATUS CODE: {status_code}'
+                         f', URL: {url}, MESSAGE: {response.text}', reference=ref)
 
     def request_to_notify(self, email, template_name, personalisation=None, reference=None):
         template_id = self._get_template_id(template_name)
