7b76842b817d8d807183ece3262113af78fc892e
bkhan2 <mohammed.khan@ons.gov.uk>
2020-04-27 13:41:42 +0100

    logging 3.7

--- response_operations_ui/controllers/notify_controller.py
+++ response_operations_ui/controllers/notify_controller.py
@@ -41,13 +41,15 @@ class NotifyController:
         url = f'{self.notify_url}{template_id}'
         auth = app.config['SECURITY_USER_NAME'], app.config['SECURITY_USER_PASSWORD']
         response = requests.post(url, auth=auth, json=notification)
+        
         try:
-            response.raise_for_status()
             logger.info('Notification id sent via Notify-Gateway to GOV.UK Notify.', id=response.json().get("id"))
+            response.raise_for_status()
         except HTTPError as e:
             ref = reference if reference else 'reference_unknown'
-            raise NotifyError('There was a problem sending a notification to Notify-Gateway',
-                              status_code=response.status_code, message=response.text, error=e, url=url) 
+            raise NotifyError('There was a problem sending a notification via Notify-Gateway to GOV.UK Notify',
+                              url=url, status_code=response.status_code,
+                              message=response.text, reference=ref, error=e) 
                               
     def request_to_notify(self, email, template_name, personalisation=None, reference=None):
         template_id = self._get_template_id(template_name)
