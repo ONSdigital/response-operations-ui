bdd2fb46c2f02bf70357c9d7fb80696ba54c2fc3
bkhan2 <mohammed.khan@ons.gov.uk>
2020-04-27 13:17:32 +0100

    logging 3.6

--- response_operations_ui/controllers/notify_controller.py
+++ response_operations_ui/controllers/notify_controller.py
@@ -47,9 +47,8 @@ class NotifyController:
         except HTTPError as e:
             ref = reference if reference else 'reference_unknown'
             raise NotifyError('There was a problem sending a notification to Notify-Gateway',
-                              status_code=response.status_code, message=response.text, error=e, url=url, 
-                              second_message='message')                              
-        
+                              status_code=response.status_code, message=response.text, error=e, url=url) 
+                              
     def request_to_notify(self, email, template_name, personalisation=None, reference=None):
         template_id = self._get_template_id(template_name)
         self._send_message(email, template_id, personalisation, reference)
