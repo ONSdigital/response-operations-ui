b36838453211b12ff01a34428d7ccb160fe70916
Amit Sinha <aks.sinha.amit@icloud.com>
2020-05-06 14:17:24 +0100

    Changes to the structlog processor to add severity for the stackdriver to log the correct severity

--- response_operations_ui/logger_config.py
+++ response_operations_ui/logger_config.py
@@ -34,6 +34,17 @@ def logger_initial_config(service_name=None,
         event_dict['service'] = service_name
         return event_dict
 
+    def add_severity_level(logger, method_name, event_dict):
+        """
+        Add the log level to the event dict.
+        """
+        if method_name == "warn":
+            # The stdlib has an alias
+            method_name = "warning"
+
+        event_dict["severity"] = method_name
+        return event_dict
+
     def zipkin_ids(logger, method_name, event_dict):
         event_dict['trace'] = ''
         event_dict['span'] = ''
@@ -48,6 +59,6 @@ def logger_initial_config(service_name=None,
         return event_dict
 
     logging.basicConfig(stream=sys.stdout, level=log_level, format=logger_format)
-    configure(processors=[zipkin_ids, add_log_level, filter_by_level, add_service, format_exc_info,
+    configure(processors=[add_severity_level, add_log_level, filter_by_level, add_service, format_exc_info,
                           TimeStamper(fmt=logger_date_format, utc=True, key="created_at"),
                           JSONRenderer(indent=indent)])
