ce056a34f3c60e882e10217558a564c6e82847e9
Warren Bailey <warren@warrenbailey.net>
2020-05-12 14:55:32 +0100

    Pinning all pipenv versions

--- tests/controllers/test_admin_controller.py
+++ tests/controllers/test_admin_controller.py
@@ -2,11 +2,17 @@ from response_operations_ui.controllers import admin_controller
 import unittest
 import fakeredis
 
+server = fakeredis.FakeServer()
+
+
+def get_fake_redis():
+    return fakeredis.FakeStrictRedis(server=server, decode_responses=True)
+
 
 class TestAdminController(unittest.TestCase):
 
     def setUp(self):
-        admin_controller._get_redis = fakeredis.FakeStrictRedis()
+        admin_controller._get_redis = get_fake_redis
 
     def test_set_banner(self):
         admin_controller.set_banner("test")
