b9c84a1f462407f21fe57f6101038f4ff4b1675f
Warren Bailey <warren@warrenbailey.net>
2020-05-14 17:14:30 +0100

    Fix unit test

--- tests/views/test_admin.py
+++ tests/views/test_admin.py
@@ -11,18 +11,42 @@ class TestAdmin(TestCase):
         self.app = create_app('TestingConfig')
         self.client = self.app.test_client()
 
-    @patch('redis.StrictRedis')
-    def test_message_does_not_show_if_redis_flag_not_set(self, mock_redis):
+    @patch('redis.Redis.get')
+    def test_message_does_not_show_if_not_set(self, mock_redis):
         mock_redis.get.return_value = b''
-        mock_redis.keys.return_value = []
         response = self.client.get('/admin/banner', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         self.assertFalse(TESTMSG in response.data)
 
-    @patch('redis.StrictRedis')
-    def test_message_shows_correct_message_if_redis_flag_set(self, mock_redis):
-        mock_redis.get.return_value = TESTMSG
-        mock_redis.keys.return_value = ['AVAILABILITY_MESSAGE']
+    @patch('redis.Redis.get')
+    def test_message_shows_correct_message_if__set(self, mock_redis):
+        mock_redis.return_value = TESTMSG
         response = self.client.get('/admin/banner', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+        mock_redis.assert_called
+        self.assertTrue(TESTMSG in response.data)
+
+    @patch('redis.Redis.get')
+    @patch('redis.Redis.set')
+    def test_message_is_set(self, mock_redis_set, mock_redis_get):
+        mock_redis_get.return_value = TESTMSG
+        response = self.client.post('/admin/banner', data=dict(
+            delete="false",
+            banner=TESTMSG,
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        mock_redis_set.set.assert_called
+        mock_redis_get.get.assert_called
+        self.assertTrue(TESTMSG in response.data)
+
+    @patch('redis.Redis.delete')
+    @patch('redis.Redis.get')
+    def test_message_is_deleted(self, mock_redis_delete, mock_redis_get):
+        response = self.client.post('/admin/banner', data=dict(
+            delete="true",
+            banner="",
+        ), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        mock_redis_delete.set.assert_called
+        mock_redis_get.get.assert_called
         self.assertFalse(TESTMSG in response.data)
