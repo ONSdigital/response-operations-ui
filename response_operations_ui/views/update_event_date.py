58a0965cb16cfc5d73b9eff318bf16d729f1e0ab
Amit Sinha <aks.sinha.amit@icloud.com>
2020-05-20 21:31:43 +0100

    update

--- response_operations_ui/views/update_event_date.py
+++ response_operations_ui/views/update_event_date.py
@@ -112,11 +112,13 @@ def update_event_date_submit(short_name, period, tag):
     """Attempts to create the event, returns None if success or returns an error message upon failure."""
     error_message = collection_exercise_controllers.update_event(
         collection_exercise_id=exercise['id'], tag=tag, timestamp=submitted_dt)
-
+    
     if error_message:
         flash(error_message, 'error')
         return redirect(url_for('collection_exercise_bp.update_event_date',
                                 short_name=short_name, period=period, tag=tag))
-
+    success_message = 'Event date updated.' if tag != 'return_by' else 'Event date updated. Nudge emails that were ' \
+                                                                       'scheduled after the return by date will be ' \
+                                                                       'removed. '
     return redirect(url_for('collection_exercise_bp.view_collection_exercise',
-                            short_name=short_name, period=period, success_panel='Event date updated.'))
+                            short_name=short_name, period=period, success_panel=success_message))
