0f8a3fb6939a729facad9edc7608f7bafdd537b6
Amit Sinha <aks.sinha.amit@icloud.com>
2020-05-22 22:00:44 +0100

    removing test mode

--- config.py
+++ config.py
@@ -72,7 +72,7 @@ class Config(object):
     CREATE_ACCOUNT_EMAIL_TOKEN_EXPIRY = int(os.getenv('CREATE_ACCOUNT_EMAIL_TOKEN_EXPIRY', '3628800'))
     CREATE_ACCOUNT_ADMIN_PASSWORD = os.getenv('CREATE_ACCOUNT_ADMIN_PASSWORD')
     
-    TEST_MODE = strtobool(os.getenv('TEST_MODE', 'True'))
+    TEST_MODE = strtobool(os.getenv('TEST_MODE', 'False'))
     WTF_CSRF_ENABLED = strtobool(os.getenv('WTF_CSRF_ENABLED', 'True'))
 
 
