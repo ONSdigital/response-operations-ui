fc1dd01162814e9830b473bc93c3eed8b20e7586
Warren Bailey <warren@warrenbailey.net>
2020-08-04 14:41:21 +0100

    Send sample file to sample file uploader

--- response_operations_ui/controllers/sample_controllers.py
+++ response_operations_ui/controllers/sample_controllers.py
@@ -32,22 +32,20 @@ def get_sample_summary(sample_summary_id):
 def upload_sample(short_name, period, file):
     logger.info('Uploading sample', short_name=short_name, period=period, filename=file.filename)
 
-    survey_type = 'B'
-    url = f'{app.config["SAMPLE_URL"]}/samples/{survey_type}/fileupload'
+    url = f'{app.config["SAMPLE_FILE_UPLOAD_URL"]}/samples/fileupload'
     response = requests.post(url=url, auth=app.config['BASIC_AUTH'], files={'file': file})
 
     try:
         response.raise_for_status()
     except (HTTPError, RequestException):
         log_level = logger.warning if response.status_code in (400, 404) else logger.exception
-        log_level('Error uploading sample file', status=response.status_code, survey_type=survey_type)
+        log_level('Error uploading sample file', status=response.status_code)
         raise ApiError(response)
 
     sample_summary = response.json()
 
     logger.info('Successfully uploaded sample file',
                 response_json=sample_summary,
-                survey_type=survey_type,
                 sample_id=sample_summary['id'])
 
     return sample_summary
