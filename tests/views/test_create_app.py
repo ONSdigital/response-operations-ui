99d71d581e54c2aeb6e5806daa4760512f28a592
Adam Mann <manny160389@hotmail.com>
2020-08-05 10:47:32 +0100

    Remove cloudfoundry code

--- tests/views/test_create_app.py
+++ tests/views/test_create_app.py
@@ -17,15 +17,3 @@ class TestCreateApp(unittest.TestCase):
     def test_create_app(self):
         test_app = create_app('TestingConfig')
         self.assertEqual(test_app.config['REDIS_HOST'], 'localhost')
-
-    @patch('response_operations_ui.cf')
-    def test_create_app_with_cf(self, mock_cf):
-        mock_cf.detected.return_value = False
-        mock_cf.redis.credentials = {
-            'host': 'test_host',
-            'port': 'test_port'
-        }
-
-        test_app = create_app()
-        self.assertEqual(test_app.config['REDIS_HOST'], 'test_host')
-        self.assertEqual(test_app.config['REDIS_PORT'], 'test_port')
