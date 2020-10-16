de88ebb419a2ff30622ede9a653be9cad4f7ba1e
Amit Sinha <aks.sinha.amit@icloud.com>
2020-10-16 11:00:10 +0100

    Changes to fix the issue with X-Forward-Proto not supported

--- response_operations_ui/__init__.py
+++ response_operations_ui/__init__.py
@@ -39,7 +39,7 @@ class GCPLoadBalancer:
         self.app = app
 
     def __call__(self, environ, start_response):
-        scheme = environ.get("HTTP_X_FORWARDED_PROTO", "http")
+        scheme = environ.get("SCHEME", "http")
         if scheme:
             environ["wsgi.url_scheme"] = scheme
         return self.app(environ, start_response)
