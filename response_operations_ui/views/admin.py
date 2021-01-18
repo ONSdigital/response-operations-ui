54ee7aff58c843ddaa6392b031ebfd4064d0dca8
Adam Mann <manny160389@hotmail.com>
2021-01-18 16:33:42 +0000

    Revert "Banner management (#569)"
    This reverts commit 56b8ebccf7146a8de3e1578b630439ec1616e68d.

--- response_operations_ui/views/admin.py
+++ response_operations_ui/views/admin.py
@@ -1,12 +1,14 @@
 import logging
-from flask import Blueprint, render_template, request, url_for, redirect
+from datetime import datetime
+
+
+from flask import Blueprint, render_template, request, url_for, redirect, flash
 from flask_login import login_required, current_user
 from structlog import wrap_logger
-import json
+from dateutil import parser
 
 from response_operations_ui.controllers import admin_controller
-from response_operations_ui.controllers.admin_controller import get_all_banners, create_new_banner, Banner
-from response_operations_ui.controllers.admin_controller import get_a_banner, edit_banner, delete_banner
+from response_operations_ui.controllers.admin_controller import get_alert_list
 from response_operations_ui.forms import BannerAdminForm
 
 logger = wrap_logger(logging.getLogger(__name__))
@@ -17,7 +19,6 @@ INFO_MESSAGES = {
     'survey_changed': "Survey details changed",
     'instrument_linked': "Collection exercise linked to survey successfully"
 }
-title_to_edit = ""
 
 
 @admin_bp.route('/banner', methods=['GET'])
@@ -27,7 +28,7 @@ def banner_admin():
     current_banner = admin_controller.current_banner()
     logger.debug("Banner page accessed", user=current_username())
     form = BannerAdminForm(form=request.form)
-    dict_of_alerts = get_all_banners()
+    dict_of_alerts = get_alert_list()
     return render_template('banner-admin.html',
                            form=form,
                            list_of_alerts=dict_of_alerts,
@@ -45,22 +46,27 @@ def current_username():
 @admin_bp.route('/banner', methods=['POST'])
 @login_required
 def update_banner():
+    breadcrumbs = [{"text": "Banner Admin", "url": ""}]
+    dict_of_alerts = get_alert_list()
+    current_banner = admin_controller.current_banner()
+    logger.debug("Updating banner", user=current_username())
     form = BannerAdminForm(form=request.form)
-    banner_id = form.banner_id.data
-    logger.info("Setting an active banner", user=current_username(), banner_id=banner_id)
-    if banner_id:
-        admin_controller.set_live_banner(banner_id)
-        return redirect(url_for("admin_bp.banner_admin"))
+    banner = form.banner.data
+    logger.debug("Banner update", user=current_username(), banner=banner)
+    if banner:
+        admin_controller.set_banner_and_time(form.banner.data, datetime.now())
+        return redirect(url_for("admin_bp.remove_alert"))
     else:
         response_error = True
     return render_template('banner-admin.html',
                            form=form,
-                           list_of_alerts=get_all_banners(),
-                           breadcrumbs=[{"text": "Banner Admin", "url": ""}],
-                           current_banner=admin_controller.current_banner(),
+                           list_of_alerts=dict_of_alerts,
+                           breadcrumbs=breadcrumbs,
+                           current_banner=current_banner,
                            response_error=response_error)
 
 
+# Parser.parse allows the string returned from redis to be converted into a datetime object from string
 @admin_bp.route('/banner/remove', methods=['GET'])
 @login_required
 def view_and_remove_current_banner():
@@ -69,92 +75,30 @@ def view_and_remove_current_banner():
     logger.debug("Deleting alert", user=current_username())
     form = BannerAdminForm(form=request.form)
     current_banner = admin_controller.current_banner()
+    time_banner_set = admin_controller.banner_time_get()
+    time_banner_set = parser.parse(time_banner_set)\
+        .strftime('%d' + set_suffix(datetime.today().day) + ' %B ' + '%Y ' + 'at %H' + ':%M')
     if current_banner:
-        return render_template('admin-remove-alert.html',
+        return render_template('remove-alert.html',
                                form=form,
                                current_banner=current_banner,
-                               breadcrumbs=breadcrumbs)
+                               breadcrumbs=breadcrumbs,
+                               time_banner_set=time_banner_set)
     else:
         return redirect(url_for("admin_bp.banner_admin"))
 
 
-def remove_alert(banner_id):
-    logger.debug("Removing banner", user=current_username())
-    delete_banner(banner_id)
+@admin_bp.route('/banner/remove', methods=['POST'])
+@login_required
+def remove_alert():
+    logger.debug("Updating banner", user=current_username())
+    form = BannerAdminForm(form=request.form)
+    delete = form.delete.data
+    if delete:
+        flash('The alert has been removed')
+        logger.debug("Banner deleted", user=current_username())
+        admin_controller.remove_banner()
     return redirect(url_for("admin_bp.banner_admin"))
-
-
-@admin_bp.route('/banner/manage', methods=['GET'])
-@login_required
-def manage_alert():
-    logger.debug("Managing banner", user=current_username())
-    form = BannerAdminForm(form=request.form)
-    list_of_alerts = get_all_banners()
-    return render_template('admin-manage.html',
-                           form=form,
-                           list_of_alerts=list_of_alerts)
-
-
-# Nothing is being posted to this endpoint. I believe its something to do with the Radio buttons and setting
-# the "name" variable to "title" at the wrong point in admin-manage.html
-@admin_bp.route('/banner/manage', methods=['POST'])
-@login_required
-def manage_alert_to_edit():
-    logger.debug("Managing banner", user=current_username())
-    id = request.form['event']
-    logger.info("form id", banner=id)
-    return redirect(url_for("admin_bp.get_banner_edit", banner_id=id))
-
-
-@admin_bp.route('/banner/edit/<banner_id>', methods=['GET'])
-@login_required
-def get_banner_edit(banner_id):
-    logger.info("searching for banner", id=banner_id)
-    banner = get_a_banner(banner_id)
-    logger.info("got banner", banner=banner)
-    form = BannerAdminForm(form=request.form)
-    return render_template('admin-edit.html',
-                           form=form,
-                           banner=banner)
-
-
-@admin_bp.route('/banner/edit/<banner_id>', methods=['POST'])
-@login_required
-def edit_the_chosen_banner(banner_id):
-    logger.debug("Editing banner", user=current_username())
-    form = BannerAdminForm(form=request.form)
-    if form.delete.data:
-        return remove_alert(banner_id)
-    title = form.title.data
-    content = form.banner.data
-    edit_banner(json.dumps({"id": banner_id, "title": title, "content": content}))
-    return redirect(url_for("admin_bp.banner_admin"))
-
-
-# Loads manage page
-@admin_bp.route('/banner/create', methods=['GET'])
-@login_required
-def create_a_new_banner():
-    logger.debug("Creating template", user=current_username())
-    form = BannerAdminForm(form=request.form)
-    return render_template('admin-create-template.html',
-                           form=form)
-
-
-@admin_bp.route('/banner/create', methods=['POST'])
-@login_required
-def put_new_banner_in_datastore():
-    logger.debug("Creating template", user=current_username())
-    form = BannerAdminForm(form=request.form)
-    title = form.title.data
-    banner = form.banner.data
-    new_banner = Banner(title, banner)
-    new_banner = new_banner.to_json()
-    try:
-        create_new_banner(new_banner)
-        return redirect(url_for("admin_bp.banner_admin"))
-    except ValueError:
-        raise ValueError
 
 
 # Currently datetime.strftime does not support applying suffix's onto the date.
