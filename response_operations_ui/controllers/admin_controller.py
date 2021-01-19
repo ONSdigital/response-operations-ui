dd695d8316a1a45e3672d4f9c64f3d86448ae26b
Adam Mann <manny160389@hotmail.com>
2021-01-19 08:54:15 +0000

    Revert "Banner management" (#584)
    * Revert "Banner management (#569)"
    
    This reverts commit 56b8ebccf7146a8de3e1578b630439ec1616e68d.
    
    * Update chart version

--- response_operations_ui/controllers/admin_controller.py
+++ response_operations_ui/controllers/admin_controller.py
@@ -1,141 +1,68 @@
+import redis
 import logging
 import json
-import requests
 
-from flask import current_app as app
+from flask import current_app
 from structlog import wrap_logger
-
-from response_operations_ui.exceptions.exceptions import ApiError
 
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-class Banner:
-    def __init__(self, title, content, active=False):
-        self.title = title
-        self.content = content
-        self.active = active
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
+def _get_redis():
+    r = redis.Redis(host=current_app.config['REDIS_HOST'],
+                    port=current_app.config['REDIS_PORT'],
+                    db=current_app.config['REDIS_DB'],
+                    decode_responses=True)
+    return r
 
 
-def set_live_banner(banner_id):
-    logger.info('Attempting to set banner to acitve', banner_id=banner_id)
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner/{banner_id}/active"
-    response = requests.patch(url)
+def set_banner_and_time(banner, time):
     try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to retrieve Banner from api')
-        raise ApiError(response)
+        r = _get_redis()
+        r.set('AVAILABILITY_MESSAGE', banner)
+        r.set('AVAILABILITY_MESSAGE_TIME_SET', time)
+        logger.debug("Setting availability message", banner=banner)
+    except redis.RedisError:
+        logger.exception("Unable to update banner and time. Ensure time parameter is correct structure if not\
+                         default. e.g. strftime does not work on string")
 
-    logger.info('Successfully retrieved current live banner from api')
-    banner = response.json()
-    return banner
 
-
-def remove_banner(banner_id):
-    logger.info('Attempting to remove banner', banner_id=banner_id)
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner/{banner_id}"
-    response = requests.delete(url)
+def remove_banner():
     try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to remove Banner', banner_id=banner_id)
-        raise ApiError(response)
-
-    logger.info('Successfully removed banner from api', banner_id=banner_id)
+        r = _get_redis()
+        r.delete('AVAILABILITY_MESSAGE')
+        logger.debug("Deleting availability message")
+    except redis.RedisError:
+        logger.exception("Unable to remove banner")
 
 
 def current_banner():
-    logger.info('Attempting to retrieve the current live banner')
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner/active"
-    response = requests.get(url)
     try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to retrieve Banner from api')
-        raise ApiError(response)
-
-    logger.info('Successfully retrieved current live banner from api')
-    if response.status_code == 204:
-        return {}
-    banner = response.json()
-    return banner
+        r = _get_redis()
+        banner = r.get('AVAILABILITY_MESSAGE')
+        logger.debug("Getting availability message", banner=banner)
+        return banner
+    except redis.RedisError:
+        logger.exception("Unable to retrieve current banners")
 
 
-def get_all_banners():
-    logger.info('Attempting to retrieve banners')
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner"
-    response = requests.get(url)
+def banner_time_get():
     try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to retrieve Banners')
-        raise ApiError(response)
-
-    list_of_banners = response.json()
-    logger.info('Successfully retrieved banners', banners=list_of_banners)
-    return list_of_banners
+        r = _get_redis()
+        banner = r.get('AVAILABILITY_MESSAGE_TIME_SET')
+        logger.debug("Getting time availability message was set", banner=banner)
+        return banner
+    except redis.RedisError:
+        logger.exception("Unable to retrieve current banners")
 
 
-def get_a_banner(banner_id):
-    logger.info('Attempting to retrieve banner', banner_id=banner_id)
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner/{banner_id}"
-    response = requests.get(url)
+def get_alert_list():
+    my_dict = {}
     try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to retrieve Banner from api')
-        raise ApiError(response)
-
-    banner = response.json()
-    logger.info('Successfully retrieved banner from api', banner=banner)
-    return banner
-
-
-def create_new_banner(banner):
-    logger.info('Attempting to store a banner banner', banner=banner)
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner"
-    headers = {'Content-type': 'application/json'}
-    response = requests.post(url, banner, headers=headers)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to create Banner')
-        raise ApiError(response)
-
-    banner = response.json()
-    logger.info('Successfully created the new Banner', banner=banner)
-    return banner
-
-
-def edit_banner(banner):
-    logger.info('Attempting to edit the banner', banner=banner)
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner"
-    headers = {'Content-type': 'application/json'}
-    response = requests.put(url, banner, headers=headers)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to edit banner via banner-api')
-        raise ApiError(response)
-
-    banner = response.json()
-    logger.info('Successfully edited the Banner', banner=banner)
-    return banner
-
-
-def delete_banner(banner_id):
-    logger.info('Attempting to delete banner from Datastore')
-    url = f"{app.config['BANNER_SERVICE_URL']}/banner/{banner_id}"
-    response = requests.delete(url)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logger.error('Failed to delete Banners from Datastore')
-        raise ApiError(response)
-
-    logger.info('Successfully deleted banner', banner_id=banner_id)
+        with open('response_operations_ui/templates/banner-admin-json.json', 'r') as f:
+            alert_list = json.load(f)
+    except (OSError, IOError) as e:
+        logger.exception(e, 'error opening JSON file containing the alert templates')
+    for i in alert_list:
+        my_dict.update(i)
+    return my_dict
