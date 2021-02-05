310195fc8f080df9afb026632c72678d5c6bf76e
Adam Wilkie <Adam.Wilkie@ons.gov.uk>
2021-02-05 17:23:34 +0000

    editing test data

--- tests/views/test_message.py
+++ tests/views/test_message.py
@@ -440,13 +440,13 @@ class TestMessage(ViewTestCase):
     message_form = {'body': "TEST BODY",
                     'subject': "TEST SUBJECT",
                     'hidden_survey': "ASHE",
-                    'ru_ref': "11110000019",
-                    'hidden_ru_ref': "11110000019"}
+                    'ru_ref': "49900000280",
+                    'hidden_ru_ref': "49900000280"}
     FDI_message = {'body': "AIFDI BODY",
                    'subject': "AIFDI SUBJECT",
                    'hidden_survey': "AIFDI",
-                   'ru_ref': "11110000019",
-                   'hidden_ru_ref': "11110000019"}
+                   'ru_ref': "49900000280",
+                   'hidden_ru_ref': "49900000280"}
     AIFDI_response = {
         "id": "41320b22-b425-4fba-a90e-718898f718ce",
         "shortName": "AIFDI",
