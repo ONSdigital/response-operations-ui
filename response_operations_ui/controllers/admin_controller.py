54a8ab94c0e5ade7d475fa213f5ea36d8793c4f6
Adam Mann <manny160389@hotmail.com>
2021-02-24 13:40:12 +0000

    Banner management (#586)
    * Changes for banner service api call
    
    * create new html pages for admin edit, manage and create templates, renamed remove-alert
    
    * Now getting banners from the DB
    
    * Changes to Rops to call new banner-api
    
    * Test changes that need fixing
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * Tests readded and changes to endpoint
    
    * fix lint
    
    * i give up
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * tweaks
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * call api instead of redis
    
    * finish up the banner management work
    
    * these tests are now irrelevant
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * lint
    
    * remove unused code and fix logging
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * Commit progress
    
    * Move templates to more sensible place
    
    * Checkin progress
    
    * More tidyup
    
    * Bring code more in line with new api
    
    * Add unit tests
    
    * Add more unit tests
    
    * Minor linting tweaks
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * Fix logging line
    
    * Add docstrings
    
    * Things finally work again!
    
    * Start applying polish
    
    * More polish
    
    * Checkin progress
    
    * Checkin more progress
    
    * Checkin progress
    
    * More polish and error handling
    
    * More tidyup
    
    * Tidy up naming convention
    
    * Got all the error text showing as expected
    
    * Add server side cap on banner text size
    
    * Another small change
    
    * Missed square bracket
    
    * Tidy template indentation and docstrings
    
    * More tidyup
    
    * Fix errors
    
    * PR comments
    
    * Changed 'banner' to 'alert' in content
    
    * Visual check feedback
    
    Co-authored-by: TomHolroyd <tomholroyd96@gmail.com>
    Co-authored-by: Chaitali15 <chaitu.tanawade@gmail.com>
    Co-authored-by: ras-rm-bot <65395820+ras-rm-bot@users.noreply.github.com>
    Co-authored-by: jterryons <57897360+jterryons@users.noreply.github.com>
    Co-authored-by: jterryons <john.terry@ons.gov.uk>
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>
    Co-authored-by: Warren Bailey <warren@warrenbailey.net>

--- response_operations_ui/controllers/admin_controller.py
+++ response_operations_ui/controllers/admin_controller.py
@@ -1,68 +1,205 @@
-import redis
 import logging
 import json
+import requests
 
-from flask import current_app
+from flask import current_app as app
 from structlog import wrap_logger
+
+from response_operations_ui.exceptions.exceptions import ApiError
 
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-def _get_redis():
-    r = redis.Redis(host=current_app.config['REDIS_HOST'],
-                    port=current_app.config['REDIS_PORT'],
-                    db=current_app.config['REDIS_DB'],
-                    decode_responses=True)
-    return r
+class Template:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
 
-
-def set_banner_and_time(banner, time):
-    try:
-        r = _get_redis()
-        r.set('AVAILABILITY_MESSAGE', banner)
-        r.set('AVAILABILITY_MESSAGE_TIME_SET', time)
-        logger.debug("Setting availability message", banner=banner)
-    except redis.RedisError:
-        logger.exception("Unable to update banner and time. Ensure time parameter is correct structure if not\
-                         default. e.g. strftime does not work on string")
-
-
-def remove_banner():
-    try:
-        r = _get_redis()
-        r.delete('AVAILABILITY_MESSAGE')
-        logger.debug("Deleting availability message")
-    except redis.RedisError:
-        logger.exception("Unable to remove banner")
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
 
 
 def current_banner():
+    """
+    Gets the current banner, if it exists.
+
+    :return: A dict with the current text of the banner. Will be empty if there isn't one set
+    :rtype dict
+    :raises ApiError: Raised on any non-404 failure status code returned from the banner-api
+    """
+    logger.info('Attempting to retrieve the current live banner')
+    url = f"{app.config['BANNER_SERVICE_URL']}/banner"
+    response = requests.get(url)
     try:
-        r = _get_redis()
-        banner = r.get('AVAILABILITY_MESSAGE')
-        logger.debug("Getting availability message", banner=banner)
-        return banner
-    except redis.RedisError:
-        logger.exception("Unable to retrieve current banners")
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        if response.status_code == 404:
+            logger.info('No banner currently active')
+            return {}
+        logger.error('Failed to retrieve current live banner')
+        raise ApiError(response)
+
+    logger.info('Successfully retrieved current live banner')
+    return response.json()
 
 
-def banner_time_get():
+def set_banner(banner_text):
+    """
+    Sets the text of the banner.  If there was a banner already active then this will overwrite the text
+    that was previously there.
+
+    :param banner_text: The text the banner will display
+    :type banner_text: str
+    :return: A copy of what the banner-api has saved to the database
+    :rtype: dict
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to set banner text', banner_text=banner_text)
+    url = f"{app.config['BANNER_SERVICE_URL']}/banner"
+    response = requests.post(url, json={'content': banner_text})
     try:
-        r = _get_redis()
-        banner = r.get('AVAILABILITY_MESSAGE_TIME_SET')
-        logger.debug("Getting time availability message was set", banner=banner)
-        return banner
-    except redis.RedisError:
-        logger.exception("Unable to retrieve current banners")
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to set banner text')
+        raise ApiError(response)
+
+    logger.info('Successfully set banner text')
+    banner = response.json()
+    return banner
 
 
-def get_alert_list():
-    my_dict = {}
+def remove_banner():
+    """
+    Deletes the banner, if it exists.  If there was no active banner then a success is reported regardless.
+
+    :rtype: None
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to remove banner')
+    url = f"{app.config['BANNER_SERVICE_URL']}/banner"
+    response = requests.delete(url)
     try:
-        with open('response_operations_ui/templates/banner-admin-json.json', 'r') as f:
-            alert_list = json.load(f)
-    except (OSError, IOError) as e:
-        logger.exception(e, 'error opening JSON file containing the alert templates')
-    for i in alert_list:
-        my_dict.update(i)
-    return my_dict
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to remove Banner')
+        raise ApiError(response)
+
+    logger.info('Successfully removed banner')
+
+
+def get_templates():
+    """
+    Gets all the templates stored in the banner-api service.
+
+    :return: A list of dicts containing the templates stored
+    :rtype: list of dict
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to retrieve templates')
+    url = f"{app.config['BANNER_SERVICE_URL']}/template"
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to retrieve templates')
+        raise ApiError(response)
+
+    templates = response.json()
+    logger.info('Successfully retrieved templates', template_count=len(templates))
+    return templates
+
+
+def get_template(template_id):
+    """
+    Get a specific template, by id, from the banner-api service.
+
+    :param template_id: A string representation of the template_id
+    :type template_id: str
+    :return: A dict containing the data stored for the template
+    :rtype: dict
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to retrieve template', template_id=template_id)
+    url = f"{app.config['BANNER_SERVICE_URL']}/template/{template_id}"
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to retrieve template', template_id=template_id)
+        raise ApiError(response)
+
+    template = response.json()
+    logger.info('Successfully retrieved template', template_id=template_id)
+    return template
+
+
+def create_new_template(template):
+    """
+    Creates a new template.
+
+    :param template: A dictionary containing all the data required for a new template
+    :type template: dict
+    :return: A copy of what the banner-api has saved to the database
+    :rtype: dict
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to create a template', template=template)
+    url = f"{app.config['BANNER_SERVICE_URL']}/template"
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, template, headers=headers)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to create template', template=template)
+        raise ApiError(response)
+
+    banner = response.json()
+    logger.info('Successfully created a template', template=template)
+    return banner
+
+
+def edit_template(template):
+    """
+    Edits an existing template. The template that is provided will overwrite everything that was there
+    previously.
+
+    :param template: A dict containing all the fields for the template
+    :type template: dict
+    :return: A copy of what the banner-api has saved to the database
+    :rtype: dict
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to edit the template', template=template)
+    url = f"{app.config['BANNER_SERVICE_URL']}/template"
+    response = requests.put(url, json=template)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to edit the template')
+        raise ApiError(response)
+
+    banner = response.json()
+    logger.info('Successfully edited the template', template=template)
+    return banner
+
+
+def delete_template(template_id):
+    """
+    Deletes a template, if it exists.  Will report success even if it didn't exist.
+
+    :param template_id: A string representation of the template_id
+    :type template_id: str
+    :rtype: None
+    :raises ApiError: Raised on any 4XX or 5XX returned from the banner-api
+    """
+    logger.info('Attempting to delete template', template_id=template_id)
+    url = f"{app.config['BANNER_SERVICE_URL']}/template/{template_id}"
+    response = requests.delete(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logger.error('Failed to delete template', template_id=template_id)
+        raise ApiError(response)
+
+    logger.info('Successfully deleted template', template_id=template_id)
--- tests/controllers/test_admin_controller.py
+++ tests/controllers/test_admin_controller.py
@@ -1,38 +1,155 @@
-from datetime import datetime
-from response_operations_ui.controllers import admin_controller
 import unittest
-import fakeredis
 
-server = fakeredis.FakeServer()
+import responses
 
+from config import TestingConfig
+from response_operations_ui import create_app
+from response_operations_ui.controllers import admin_controller
+from response_operations_ui.exceptions.exceptions import ApiError
 
-def get_fake_redis():
-    return fakeredis.FakeStrictRedis(server=server, decode_responses=True)
+url_banner = f'{TestingConfig.BANNER_SERVICE_URL}/banner'
+url_template = f'{TestingConfig.BANNER_SERVICE_URL}/template'
+
+banner_response = {'id': 'active', 'content': 'This is some text'}
+set_banner_request = {'content': 'This is some text'}
+
+template_1 = {'id': 1, 'title': 'Banner title 1', 'content': 'Banner content 1'}
+template_2 = {'id': 2, 'title': 'Banner title 2', 'content': 'Banner content 2'}
+templates_response = [template_1, template_2]
 
 
 class TestAdminController(unittest.TestCase):
 
     def setUp(self):
-        admin_controller._get_redis = get_fake_redis
+        self.app = create_app('TestingConfig')
+        self.client = self.app.test_client()
 
-    def test_set_banner(self):
-        admin_controller.set_banner_and_time('test', datetime.now())
-        self.assertEqual("test", admin_controller.current_banner())
+    # Banner tests
 
-    def test_remove_banner(self):
-        admin_controller.set_banner_and_time("test", datetime.now())
-        admin_controller.remove_banner()
-        self.assertIsNone(admin_controller.current_banner())
+    def test_get_banner_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, url_banner, json=banner_response, status=200, content_type='application/json')
+            with self.app.app_context():
+                response = admin_controller.current_banner()
+                self.assertEqual(response, banner_response)
 
-    def test_current_time_set(self):
-        test_time = datetime.now()
-        admin_controller.set_banner_and_time("test", test_time)
-        self.assertEqual(str(test_time), admin_controller.banner_time_get())
+    def test_get_banner_not_found(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, url_banner, status=404)
+            with self.app.app_context():
+                response = admin_controller.current_banner()
+                expected = {}
+                self.assertEqual(response, expected)
 
-    # Placeholder test pending implementation of banner message storage:
-    def test_get_alert_list(self):
-        test_dict = admin_controller.get_alert_list()
-        self.assertIn("Unexpected outage", test_dict.keys())
-        self.assertIn("The Quarterly Vacancy Survey is unavailable until Tuesday 7 July 2020,"
-                      " as we are currently experiencing some technical difficulties.",
-                      test_dict.values())
+    def test_get_banner_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, url_banner, status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.current_banner()
+
+    def test_set_banner_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.POST, url_banner, json=banner_response, status=201, content_type='application/json')
+            with self.app.app_context():
+                response = admin_controller.set_banner(set_banner_request)
+                self.assertEqual(response, banner_response)
+
+    def test_set_banner_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.POST, url_banner, status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.set_banner(set_banner_request)
+
+    def test_remove_banner_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.DELETE, url_banner, status=204)
+            with self.app.app_context():
+                self.assertIsNone(admin_controller.remove_banner())
+
+    def test_remove_banner_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.DELETE, url_banner, status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.remove_banner()
+
+    # Template tests
+
+    def test_get_templates_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, url_template, json=templates_response, status=200, content_type='application/json')
+            with self.app.app_context():
+                response = admin_controller.get_templates()
+                self.assertEqual(response, templates_response)
+
+    def test_get_templates_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, url_template, status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.get_templates()
+
+    def test_get_template_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, f"{url_template}/1", json=template_1, status=200, content_type='application/json')
+            with self.app.app_context():
+                response = admin_controller.get_template("1")
+                self.assertEqual(response, template_1)
+
+    def test_get_template_not_found(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, f"{url_template}/1", status=404)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.get_template("1")
+
+    def test_get_template_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.GET, f"{url_template}/1", status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.get_template("1")
+
+    def test_edit_template_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.PUT, url_template, json=template_2, status=200, content_type='application/json')
+            with self.app.app_context():
+                response = admin_controller.edit_template(template_2)
+                self.assertEqual(response, template_2)
+
+    def test_edit_template_bad_request(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.PUT, url_template, status=400)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.edit_template(template_2)
+
+    def test_edit_template_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.PUT, url_template, status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.edit_template(template_2)
+
+    def test_delete_template_success(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.DELETE, f"{url_template}/1", status=204)
+            with self.app.app_context():
+                self.assertIsNone(admin_controller.delete_template("1"))
+
+    def test_delete_template_bad_request(self):
+        """ Bad request happens if the id isn't a number"""
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.DELETE, f"{url_template}/abc", status=400)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.delete_template("abc")
+
+    def test_delete_template_server_error(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(rsps.DELETE, f"{url_template}/1", status=500)
+            with self.app.app_context():
+                with self.assertRaises(ApiError):
+                    admin_controller.delete_template("1")
