263b027c273e8d81854bf9f5c5eb8ade03e848ad
Warren Bailey <warren@warrenbailey.net>
2021-05-26 14:44:06 +0100

    fix is needed in create and update event code

--- response_operations_ui/views/update_event_date.py
+++ response_operations_ui/views/update_event_date.py
@@ -53,7 +53,7 @@ def update_event_date(short_name, period, tag):
                            event_name=event_name,
                            date_restriction_text=date_restriction_text,
                            show=show,
-                           event_tag=tag)
+                           tag=tag)
 
 
 def is_viewed_reminder_last_in_sequence(events, tag):
