e3c3d17848bac4e4d65a8277cd92dfd795841d38
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2022-02-04 16:13:40 +0000

    adding tests (no_selection made does not work)

--- tests/views/test_accounts.py
+++ tests/views/test_accounts.py
@@ -14,6 +14,8 @@ from response_operations_ui.common import token_decoder
 project_root = os.path.dirname(os.path.dirname(__file__))
 with open(f"{project_root}/test_data/uaa/user_by_id.json") as json_data:
     uaa_user_by_id_json = json.load(json_data)
+with open(f"{project_root}/test_data/uaa/updated_name.json") as json_data:
+    updated_name_json = json.load(json_data)
 
 test_email = "fake@ons.gov.uk"
 user_id = "fe2dc842-b3b3-4647-8317-858dab82ab94"
@@ -21,6 +23,7 @@ url_uaa_token = f"{TestingConfig.UAA_SERVICE_URL}/oauth/token"
 url_uaa_get_accounts = f"{TestingConfig.UAA_SERVICE_URL}/Users?filter=email+eq+%22{test_email}%22"
 url_uaa_get_user_by_id = f"{TestingConfig.UAA_SERVICE_URL}/Users/{user_id}"
 url_uaa_create_account = f"{TestingConfig.UAA_SERVICE_URL}/Users"
+url_uaa_update_name = f"{TestingConfig.UAA_SERVICE_URL}/Users/{user_id}"
 
 
 class TestAccounts(unittest.TestCase):
@@ -37,22 +40,6 @@ class TestAccounts(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     # Uncomment once my-account page is enabled in future PR
-    # @requests_mock.mock()
-    # def test_my_account_page(self, mock_request):
-    #     with self.client.session_transaction() as session:
-    #         session["user_id"] = user_id
-    #     mock_request.post(url_uaa_token, json={"access_token": self.access_token}, status_code=201)
-    #     mock_request.get(url_uaa_get_user_by_id, json=uaa_user_by_id_json, status_code=200)
-    #     response = self.client.get("/account/my-account", follow_redirects=True)
-    #
-    #     self.assertIn(b"Email address:", response.data)
-    #     self.assertIn(b"ons@ons.fake", response.data)
-    #     self.assertIn(b"Username:", response.data)
-    #     self.assertIn(b"uaa_user", response.data)
-    #     self.assertIn(b"Name:", response.data)
-    #     self.assertIn(b"ONS User", response.data)
-    #     self.assertEqual(response.status_code, 200)
-
     @requests_mock.mock()
     def test_my_account_page(self, mock_request):
         with self.client.session_transaction() as session:
@@ -68,6 +55,44 @@ class TestAccounts(unittest.TestCase):
         self.assertIn(b"Name:", response.data)
         self.assertIn(b"ONS User", response.data)
         self.assertEqual(response.status_code, 200)
+
+    @requests_mock.mock()
+    def test_no_selection_made(self, mock_request):
+        with self.client.session_transaction() as session:
+            session["user_id"] = user_id
+        mock_request.post(url_uaa_token, json={"access_token": self.access_token}, status_code=201)
+        mock_request.get(url_uaa_get_user_by_id, json=uaa_user_by_id_json, status_code=200)
+        response = self.client.get("account/my-account", data=None, follow_redirects=True)
+        self.assertIn(b"You need to choose and option", response.data)
+
+    @requests_mock.mock()
+    def test_change_account_name_page(self, mock_request):
+        with self.client.session_transaction() as session:
+            session["user_id"] = user_id
+        mock_request.post(url_uaa_token, json={"access_token": self.access_token}, status_code=201)
+        mock_request.get(url_uaa_get_user_by_id, json=uaa_user_by_id_json, status_code=200)
+        response = self.client.get("/account/change-account-name", follow_redirects=True)
+        self.assertIn(b"First name", response.data)
+        self.assertIn(b"ONS", response.data)
+        self.assertIn(b"Last name", response.data)
+        self.assertIn(b"User", response.data)
+        self.assertEqual(response.status_code, 200)
+
+    @requests_mock.mock()
+    def test_change_name(self, mock_request):
+        with patch("response_operations_ui.views.accounts.NotifyController") as mock_notify:
+            with self.client.session_transaction() as session:
+                session["user_id"] = user_id
+            mock_notify()._send_message.return_value = mock.Mock()
+            mock_request.post(url_uaa_token, json={"access_token": self.access_token}, status_code=201)
+            mock_request.get(url_uaa_get_user_by_id, json=uaa_user_by_id_json, status_code=200)
+            mock_request.put(url_uaa_update_name, status_code=200)
+            response = self.client.post(
+                "/account/change-account-name",
+                follow_redirects=True,
+                data={"first_name": "ONSYES", "last_name": "Userno"},
+            )
+            self.assertIn(b"Your name has been changed", response.data)
 
     @requests_mock.mock()
     def test_request_account(self, mock_request):
