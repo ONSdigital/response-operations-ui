24bd5eede3df5f9af1ec4a06c383f47cff6a14f4
Matt <matt.crocker@gmail.com>
2022-02-25 15:25:43 +0000

    Missing token mocks

--- tests/controllers/test_uaa_controller.py
+++ tests/controllers/test_uaa_controller.py
@@ -3,6 +3,7 @@ import os
 import unittest
 from urllib.error import HTTPError
 
+import jwt
 import responses
 
 from config import TestingConfig
@@ -13,16 +14,21 @@ project_root = os.path.dirname(os.path.dirname(__file__))
 with open(f"{project_root}/test_data/uaa/user_groups.json") as json_data:
     uaa_user_groups_json = json.load(json_data)
 
+user_id = "fe2dc842-b3b3-4647-8317-858dab82ab94"
 url_uaa_get_user_groups = f"{TestingConfig.UAA_SERVICE_URL}/Groups"
+url_uaa_token = f"{TestingConfig.UAA_SERVICE_URL}/oauth/token"
 
 
 class TestUAAController(unittest.TestCase):
     def setUp(self):
+        payload = {"user_id": user_id, "aud": "response_operations"}
         self.app = create_app("TestingConfig")
         self.client = self.app.test_client()
+        self.access_token = jwt.encode(payload, self.app.config["UAA_PRIVATE_KEY"], algorithm="RS256")
 
     def test_get_user_group_list_success(self):
         with responses.RequestsMock() as rsps:
+            rsps.add(rsps.POST, url_uaa_token, json={"access_token": self.access_token}, status_code=201)
             rsps.add(
                 rsps.GET,
                 url_uaa_get_user_groups,
