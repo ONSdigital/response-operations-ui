af433390c35af48844011d29f42a59208a4fdf4c
Matt <matt.crocker@gmail.com>
2022-02-25 16:49:25 +0000

    Exception testing

--- tests/controllers/test_uaa_controller.py
+++ tests/controllers/test_uaa_controller.py
@@ -46,4 +46,5 @@ class TestUAAController(unittest.TestCase):
             rsps.add(rsps.POST, url_uaa_token, json={"access_token": self.access_token}, status=201)
             rsps.add(rsps.GET, url_uaa_get_user_groups, status=400)
             with self.app.app_context():
-                self.assertRaises(HTTPError, uaa_controller.get_user_group_list())
+                with self.assertRaises(HTTPError):
+                    uaa_controller.get_user_group_list()
