055b4fa9ac76ba9c4cf5590c75be95d09587aac3
Amit Sinha <amit@creativeappdevs.com>
2022-03-11 14:45:50 +0000

    test

--- response_operations_ui/__init__.py
+++ response_operations_ui/__init__.py
@@ -6,12 +6,12 @@ import redis
 from flask import Flask, flash, redirect, session, url_for
 from flask_assets import Environment
 from flask_login import LoginManager
+from flask_session import Session
 from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFError, CSRFProtect
 from structlog import wrap_logger
 
 from config import Config
-from flask_session import Session
 from response_operations_ui.common.jinja_filters import filter_blueprint
 from response_operations_ui.controllers.uaa_controller import user_has_permission
 from response_operations_ui.logger_config import logger_initial_config
