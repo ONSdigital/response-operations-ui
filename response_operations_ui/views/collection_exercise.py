bca4e39b4ebfd64ac52dc65d440d11118cdb0f2d
Alejandro Arroyo <alejandro.arroyo@ons.gov.uk>
2022-05-23 09:57:46 +0100

    small fix - removing flask app import for eq v3 flag

--- response_operations_ui/views/collection_exercise.py
+++ response_operations_ui/views/collection_exercise.py
@@ -5,9 +5,9 @@ from datetime import datetime
 import iso8601
 from dateutil import tz
 from dateutil.parser import parse
-from flask import Blueprint, abort
-from flask import current_app as app
 from flask import (
+    Blueprint,
+    abort,
     flash,
     jsonify,
     make_response,
