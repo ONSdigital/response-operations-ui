497942aa7b12fa57ad51c0fafbb504a449928165
Adam Mann <manny160389@hotmail.com>
2022-09-13 14:02:00 +0100

    Lint

--- response_operations_ui/controllers/sample_controllers.py
+++ response_operations_ui/controllers/sample_controllers.py
@@ -37,7 +37,10 @@ def check_if_all_sample_units_present_for_sample_summary(sample_summary_id: str)
     :return:
     """
     logger.info("Checking sample summary state", sample_summary_id=sample_summary_id)
-    url = f'{app.config["SAMPLE_URL"]}/samples/samplesummary/{sample_summary_id}/check-and-transition-sample-summary-status'
+    url = (
+        f'{app.config["SAMPLE_URL"]}/samples/samplesummary/'
+        f"{sample_summary_id}/check-and-transition-sample-summary-status"
+    )
     response = requests.get(url, auth=app.config["BASIC_AUTH"])
 
     try:
