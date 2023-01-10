24c6fb8b55f76e4349e8fa2e20ecb2b3578f7136
Matt <matt.crocker@gmail.com>
2023-01-10 17:05:22 +0000

    Oh look Black again

--- response_operations_ui/views/collection_exercise.py
+++ response_operations_ui/views/collection_exercise.py
@@ -659,9 +659,7 @@ def create_collection_exercise(survey_ref, short_name):
 
     logger.info("Successfully created collection exercise", survey=short_name, survey_ref=survey_ref)
     return redirect(
-        url_for(
-            "collection_exercise_bp.view_collection_exercise", short_name=short_name, period=form.get("period")
-        )
+        url_for("collection_exercise_bp.view_collection_exercise", short_name=short_name, period=form.get("period"))
     )
 
 
