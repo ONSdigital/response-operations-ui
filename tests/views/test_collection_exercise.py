00004c62d949a971bfc2d7a36dcccb311d4d314e
Matt <matt.crocker@gmail.com>
2023-01-18 15:54:52 +0000

    Removed duplicate test

--- tests/views/test_collection_exercise.py
+++ tests/views/test_collection_exercise.py
@@ -1043,37 +1043,6 @@ class TestCollectionExercise(ViewTestCase):
         self.assertNotIn("Incorrect file type. Please choose a file type XLSX".encode(), response.data)
 
     @requests_mock.mock()
-    @patch("response_operations_ui.views.collection_exercise.build_collection_exercise_details")
-    def test_update_collection_exercise_details_success(self, mock_request, mock_details):
-        sign_in_with_permission(self, mock_request, user_permission_surveys_edit_json)
-        changed_ce_details = {
-            "collection_exercise_id": collection_exercise_id,
-            "user_description": "16th June 2019",
-            "period": "201907",
-            "hidden_survey_id": survey_id,
-        }
-        # update survey
-        mock_details.return_value = formatted_collection_exercise_details
-        mock_request.get(url_get_survey_by_short_name, json=updated_survey_info["survey"])
-        mock_request.put(url_update_ce_user_details)
-        mock_request.put(url_update_ce_period)
-        # redirect to survey details
-        mock_request.get(url_ces_by_survey, json=updated_survey_info["collection_exercises"])
-        mock_request.get(url_get_collection_exercise_events, json=self.collection_exercise_events)
-        mock_request.get(url_get_collection_exercises_link, json=self.collection_exercises_link)
-        mock_request.get(url_get_sample_summary, json=self.sample_summary)
-
-        response = self.client.post(
-            f"/surveys/{short_name}/{period}/edit-collection-exercise-details",
-            data=changed_ce_details,
-            follow_redirects=True,
-        )
-
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("16th June 2019".encode(), response.data)
-        self.assertIn("201906".encode(), response.data)
-
-    @requests_mock.mock()
     def test_update_collection_exercise_userdescription_success(self, mock_request):
         sign_in_with_permission(self, mock_request, user_permission_surveys_edit_json)
         test_description = "16th June 2019"
