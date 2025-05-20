18d8e0ec30a6e07d8f2eec7b7533f65751840908
matthew-robinson-ons <matthew.robinson@ons.gov.uk>
2025-05-20 10:20:56 +0100

    add in feature flag and update tests to reflect

--- tests/views/test_collection_exercise.py
+++ tests/views/test_collection_exercise.py
@@ -2383,7 +2383,7 @@ class TestCollectionExercise(ViewTestCase):
         self.assertEqual(200, response.status_code)
         self.assertIn("Select EQ collection instruments".encode(), response.data)
         self.assertIn("checkbox-answer".encode(), response.data)
-        self.assertIn("Continue to choose versions".encode(), response.data)
+        self.assertIn("Done".encode(), response.data)
 
     @requests_mock.mock()
     def test_eq_view_sample_ci_page_survey_permission_with_cir(self, mock_request):
@@ -2484,7 +2484,7 @@ class TestCollectionExercise(ViewTestCase):
         self.assertEqual(200, response.status_code)
         self.assertIn("Select EQ collection instruments".encode(), response.data)
         self.assertIn("btn-add-ci".encode(), response.data)
-        self.assertIn("Continue to choose versions".encode(), response.data)
+        self.assertIn("Done".encode(), response.data)
 
     @requests_mock.mock()
     @mock.patch("response_operations_ui.views.collection_exercise.build_collection_exercise_details")
