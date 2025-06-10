399adcf1d2cdfb26832ed28a1dad36867609d715
matthew-robinson-ons <matthew.robinson@ons.gov.uk>
2025-06-10 16:45:35 +0100

    further description updates

--- response_operations_ui/controllers/collection_instrument_controllers.py
+++ response_operations_ui/controllers/collection_instrument_controllers.py
@@ -273,7 +273,7 @@ def _build_classifiers(collection_exercise_id=None, survey_id=None, ci_type=None
 def get_registry_instruments_by_exercise_id(exercise_id):
     """Retrieves a list of registry instruments for a given collection exercise.
 
-    :param exercise_id: The ID of the collection exercise.
+    :param exercise_id: The ID of the collection exercise the registry instruments are associated with.
     :type exercise_id: uuid
     :return: The registry instrument data if found, None otherwise.
     :rtype: list or None
