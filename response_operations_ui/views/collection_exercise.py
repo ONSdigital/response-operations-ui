27e211a74f151dd0bf74ac602ae554289d99e3f0
LJBabbage <ljbabbage@hotmail.com>
2025-06-12 15:32:24 +0100

    Linting

--- response_operations_ui/views/collection_exercise.py
+++ response_operations_ui/views/collection_exercise.py
@@ -6,8 +6,10 @@ from datetime import datetime
 import iso8601
 from dateutil import tz
 from dateutil.parser import parse
-from flask import Blueprint, abort
-
+from flask import (
+    Blueprint,
+    abort,
+)
 from flask import current_app as app
 from flask import (
     flash,
