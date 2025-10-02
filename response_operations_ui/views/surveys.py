02981c0484af7d4b95251e0cf8c07bfb37529d3f
Mark Price <mark.price@ons.gov.uk>
2025-10-02 08:44:11 +0100

    lint

--- response_operations_ui/views/surveys.py
+++ response_operations_ui/views/surveys.py
@@ -169,8 +169,8 @@ def _sort_collection_exercise(collection_exercises):
     collection_exercises.sort(
         key=lambda ce: (
             datetime.strptime(ce["events"]["mps"]["date"], "%d %b %Y")
-                if "mps" in ce["events"] else datetime.strptime(
-                ce["created"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            if "mps" in ce["events"]
+            else datetime.strptime(ce["created"], "%Y-%m-%dT%H:%M:%S.%fZ")
             # ce["state"],
             # datetime.strptime(ce["events"]["mps"]["date"], "%d %b %Y")
             # if "mps" in ce["events"]
